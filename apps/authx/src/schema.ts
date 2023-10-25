17c79076d63945d25eec21c8abee82487fa55817
Mario Flores <m.floresrivera@orbisops.com>
2023-10-24 23:43:06 -0400

    read user and group info
    added groupInfo endpoint
    
    added log notes
    
    can read UserInfo (-dataServices)
    
    can readGroup info (-dataServices)
    
    user can see dataServices from it's org
    
    remove logs
    
    group can see org dataServices
    
    added organizatons to group
    
    refactored runquery function
    
    more descriptive testing
    
    group testing from client
    
    unstable
    
    sleeping
    
    merged with testing
    
    remove data services from an organization and members from groups

--- apps/authx/src/schema.ts
+++ apps/authx/src/schema.ts
@@ -1,29 +1,40 @@
 import { createSchema } from 'graphql-yoga';
 import { Context } from 'hono';
-import status from "./status"
-import {AuthzedClient, ZitadelClient} from "../../../packages/authx"
+import status from './status';
+import { AuthzedClient, ZitadelClient } from '../../../packages/authx';
 
 class Status {
-	constructor(){}
+	constructor() {}
 
 	status() {
 		return {
-			health: "ok"
-		}
+			health: 'ok',
+		};
 	}
 }
 
 export default createSchema({
-    typeDefs: `
+	typeDefs: `
     type Query {
         health: String!
         status: Status!
+        user(userId: String!): UserInfo!
+		group(groupId: String!): GroupInfo!
         validateUser(token: String!): UserValidation
         listUsersInOrganization(orgId: String!): [String!]!
     }
 
     type Mutation {
         addUserToOrganization(orgId: String!, userId: String!): Boolean!
+        addOwnerToOrganization(orgId: String!, userId: String!): Boolean!
+        addDataServiceToOrganization(orgId: String!, dataServiceId: String!): Boolean!
+		adOwnerToDataService(dataServiceId: String!, userId: String!): Boolean!
+		addOrganizationToGroup(organizationId: String!, groupId: String!): Boolean!
+		addUserToGroup(userId: String!, groupId: String!): Boolean!
+		addOwnerToGroup(userId: String!, groupId: String!): Boolean!
+		addServiceAccountToGroup(serviceAccountId: String!, groupId: String!): Boolean!
+		removeUserFromGroup(userId: String!, groupId: String!): Boolean!
+		removeDataServiceFromOrganization(dataServiceId: String!, orgId: String!): Boolean!
     }
 
     type Status {
@@ -40,48 +51,160 @@ export default createSchema({
         subject: AuthzedObject
         resource: AuthzedObject
     }
+    type UserInfo {
+        groups: [String]
+        organizations: [String]
+		ownedGroups: [String]
+		ownedOrganizations: [String]
+        dataServices: [String]
+        ownedDataServices: [String]
+    }
+	type GroupInfo {
+		users: [String]
+		serviceAccounts: [String]
+		dataServices: [String]
+		organizations: [String]
+	}
     type AuthzedObject {
         objectType: String
         objectId: String
     }
     `,
-    resolvers: {
-        Query: {
-            health: () => "ok",
-            status: () => status.status(),
-            validateUser: async (_, {token}, context: Context) => {
-                console.log(_, token, context,);
-                const zitadelClient: ZitadelClient = context.get("zitadel");
-                const validCheck = await zitadelClient?.validateTokenByIntrospection(token, true);
-                if (validCheck === undefined || validCheck.active === false) {
-                    return {
-                        valid: false
-                    }
-                }
+	resolvers: {
+		Query: {
+			health: () => 'ok',
+			status: () => status.status(),
+			validateUser: async (_, { token }, context: Context) => {
+				console.log(_, token, context);
+				const zitadelClient: ZitadelClient = context.get('zitadel');
+				const validCheck = await zitadelClient?.validateTokenByIntrospection(token, true);
+				if (validCheck === undefined || validCheck.active === false) {
+					return {
+						valid: false,
+					};
+				}
 
-                return {
-                    valid: validCheck.active,
-                    userId: validCheck.sub,
-                    orgId: validCheck["urn:zitadel:iam:user:resourceowner:id"]
-                }
-            },
-            listUsersInOrganization: async (_, {orgId}, context: Context) => {
-                const authzedClient: AuthzedClient = context.get("authzed")
-                return authzedClient.listUsersInOrganization(orgId)
-            }
-        },
-        Mutation: {
-            addUserToOrganization: async (_, {orgId, userId}, context: Context): Promise<boolean> => {
-                const authzedClient: AuthzedClient = context.get("authzed")
-                const result  = await authzedClient.addUserToOrganization(orgId, userId);
+				return {
+					valid: validCheck.active,
+					userId: validCheck.sub,
+					orgId: validCheck['urn:zitadel:iam:user:resourceowner:id'],
+				};
+			},
+			listUsersInOrganization: async (_, { orgId }, context: Context) => {
+				const authzedClient: AuthzedClient = context.get('authzed');
+				return authzedClient.listUsersInOrganization(orgId);
+			},
+			user(_, { userId }, context: Context) {
+				const authzedClient: AuthzedClient = context.get('authzed');
+				console.log('user called with ', userId);
+				return authzedClient.getUserInfo(userId);
+			},
+			group(_, { groupId }, context: Context) {
+				const authzedClient: AuthzedClient = context.get('authzed');
+				return authzedClient.getGroupInfo(groupId);
+			},
+		},
+		Mutation: {
+			addUserToOrganization: async (_, { orgId, userId }, context: Context): Promise<boolean> => {
+				const authzedClient: AuthzedClient = context.get('authzed');
+				const result = await authzedClient.addUserToOrganization(orgId, userId);
 
-                if (result.writtenAt) {
-                    return true;
-                }
+				if (result.writtenAt) {
+					return true;
+				}
 
-                console.error(`error writing to authzed - code: ${result.code}, msg: ${result.message}`)
-                return false
-            }
-        }
-    }
-})
+				console.error(`error writing to authzed - code: ${result.code}, msg: ${result.message}`);
+				return false;
+			},
+			addOwnerToOrganization: async (_, { orgId, userId }, context: Context): Promise<boolean> => {
+				const authzedClient: AuthzedClient = context.get('authzed');
+				const result = await authzedClient.addUserToOrganization(orgId, userId, true);
+
+				if (result.writtenAt) {
+					return true;
+				}
+
+				console.error(`error writing to authzed - code: ${result.code}, msg: ${result.message}`);
+				return false;
+			},
+			addDataServiceToOrganization: async (_, { orgId, dataServiceId }, context: Context): Promise<boolean> => {
+				const authzedClient: AuthzedClient = context.get('authzed');
+				const result = await authzedClient.addDataServiceToOrganization(dataServiceId, orgId);
+
+				if (result.writtenAt) {
+					return true;
+				}
+
+				console.error(`error writing to authzed - code: ${result.code}, msg: ${result.message}`);
+				return false;
+			},
+			removeDataServiceFromOrganization: async (_, { orgId, dataServiceId }, context: Context): Promise<boolean> => {
+				const authzedClient: AuthzedClient = context.get('authzed');
+				const result = await authzedClient.removeDataServiceFromOrganization(dataServiceId, orgId);
+
+				if (result.deletedAt) {
+					return true;
+				}
+
+				console.error(`error writing to authzed - code: ${result.code}, msg: ${result.message}`);
+				return false;
+			},
+			adOwnerToDataService: async (_, { dataServiceId, userId }, context: Context): Promise<boolean> => {
+				const authzedClient: AuthzedClient = context.get('authzed');
+				const result = await authzedClient.addOwnerToDataService(userId, dataServiceId);
+
+				if (result.writtenAt) {
+					return true;
+				}
+
+				console.error(`error writing to authzed - code: ${result.code}, msg: ${result.message}`);
+				return false;
+			},
+			addOrganizationToGroup: async (_, { organizationId, groupId }, context: Context): Promise<boolean> => {
+				const authzedClient: AuthzedClient = context.get('authzed');
+				const result = await authzedClient.addOrganizationToGroup(organizationId, groupId);
+				return result.writtenAt ? true : false;
+			},
+			addUserToGroup: async (_, { groupId, userId }, context: Context): Promise<boolean> => {
+				const authzedClient: AuthzedClient = context.get('authzed');
+				const result = await authzedClient.addUserToGroup(userId, groupId);
+				if (result.writtenAt) {
+					return true;
+				}
+
+				console.error(`error writing to authzed - code: ${result.code}, msg: ${result.message}`);
+				return false;
+			},
+			removeUserFromGroup: async (_, { groupId, userId }, context: Context): Promise<boolean> => {
+				const authzedClient: AuthzedClient = context.get('authzed');
+				const result = await authzedClient.removeUserFromGroup(userId, groupId);
+				if (result.deletedAt) {
+					return true;
+				}
+
+				console.error(`error writing to authzed - code: ${result.code}, msg: ${result.message}`);
+				return false;
+			},
+			addOwnerToGroup: async (_, { groupId, userId }, context: Context): Promise<boolean> => {
+				const authzedClient: AuthzedClient = context.get('authzed');
+				const result = await authzedClient.addUserToGroup(userId, groupId, true);
+				if (result.writtenAt) {
+					return true;
+				}
+
+				console.error(`error writing to authzed - code: ${result.code}, msg: ${result.message}`);
+				return false;
+			},
+			addServiceAccountToGroup: async (_, { groupId, serviceAccountId }, context: Context): Promise<boolean> => {
+				const authzedClient: AuthzedClient = context.get('authzed');
+				const result = await authzedClient.addServiceAccountToGroup(serviceAccountId, groupId);
+				if (result.writtenAt) {
+					return true;
+				}
+
+				console.error(`error writing to authzed - code: ${result.code}, msg: ${result.message}`);
+				return false;
+			},
+		},
+	},
+});
