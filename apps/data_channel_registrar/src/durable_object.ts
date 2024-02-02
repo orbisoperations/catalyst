ee08150b4a7f210e4924d8dcf91871b981f7996e
j.taylor <j.taylor@orbisops.com>
2024-02-02 22:39:01 +0000

    d0 working

--- apps/data_channel_registrar/src/durable_object.ts
+++ apps/data_channel_registrar/src/durable_object.ts
@@ -4,22 +4,23 @@ import { DurableObjectState } from "@cloudflare/workers-types"
 type State = string
 
 export class RegistrarState {
-  registrarState: State = ""
-  d0State: DurableObjectState
-  app: Hono = new Hono()
+    state: DurableObjectState
+    app: Hono = new Hono()
+    storedConfig: State = ""
 
-  constructor(state: DurableObjectState) {
-    this.d0State = state
-    this.d0State.blockConcurrencyWhile(async () => {
-      return 
-    })
+    constructor(state: DurableObjectState) {
+        this.state = state
 
-    this.app.get('/id', async (c) => {
-      return c.text(this.d0State.id.toString())
-    })
-  }
+        this.state.blockConcurrencyWhile(async () => {
+            return
+        })
+    
+        this.app.get("/", async (c)  => { 
+            return c.json({id: ""}, 200)
+        })
+    }
 
-  async fetch(request: Request) {
-    return this.app.fetch(request)
-  }
+    async fetch(request: Request) {
+        return this.app.fetch(request)
+    }
 }
