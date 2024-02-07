388c98e4038b97abfa401846561c6b709f907bb6
j.taylor <j.taylor@orbisops.com>
2024-02-07 05:53:45 +0000

    integrating cli with api

--- apps/catalyst_cli/source/commands/datachannels/create.tsx
+++ apps/catalyst_cli/source/commands/datachannels/create.tsx
@@ -1,6 +1,21 @@
 import React from 'react';
 import {Text} from 'ink';
 import zod from 'zod';
+//import {getGraphqlClient} from "../../utils.js"
+import { GraphQLClient } from 'graphql-request'
+import { getConfig } from '../../config.js'
+
+function getGraphqlClient(): [GraphQLClient, React.JSX.Element] {
+    // @ts-ignore
+    const [config, configLogs] = getConfig();
+
+    const client = new GraphQLClient(config.catalystRegistrarEndpoint)
+
+    return [client, (<>
+    {configLogs}
+    <Text>Created GraphqlClient for endpoint {config.catalystRegistrarEndpoint}</Text>
+    </>)]
+}
 
 export const args = zod.tuple([zod.string().describe("organization name"), zod.string().describe("data channel name"), zod.string().describe("endpoint")]).describe("[organizaiton name] [channel name] [endpoint]")
 
@@ -9,5 +24,10 @@ type Props = {
 };
 
 export default function Create({args}: Props) {
-	return <Text>New data channel created for {args[0]}: {args[1]}/{args[2]}</Text>;
+    // @ts-ignore
+    const [_, clientLogs] = getGraphqlClient();
+	return (<>
+    {clientLogs}
+    <Text>New data channel created for {args[0]}: {args[1]}/{args[2]}</Text>
+    </>);
 }
--- apps/data_channel_registrar/src/pothos.ts
+++ apps/data_channel_registrar/src/pothos.ts
@@ -1,18 +1,83 @@
 import { createYoga } from 'graphql-yoga';
 import SchemaBuilder from '@pothos/core';
 import { Context } from 'hono';
+import { Parser } from 'graphql/language/parser';
 
 const builder = new SchemaBuilder({});
 
+export class DataChannel {
+    organization: string
+    name: string
+    endpoint: string
+
+    constructor(org: string, name: string, endpoint: string) {
+        this.organization = org
+        this.name = name
+        this.endpoint = endpoint
+    }
+}
+
+builder.objectType(DataChannel, {
+    name: "DataChannel",
+    description: "Data Channels are the primative for federated data",
+    fields: (t) => ({
+        organization: t.exposeString("organization", {}),
+        name: t.exposeString("name", {}),
+        endpoint: t.exposeString("endpoint", {})
+    }),
+})
+
 builder.queryType({
   fields: (t) => ({
-    hello: t.string({
-      args: {
-        name: t.arg.string(),
-      },
-      resolve: (parent, { name }) => `hello, ${name || 'World'}`,
+    listDataChannels: t.field({
+        type: [DataChannel],
+        args: {
+            organization: t.arg.string({require: true}),
+            name: t.arg.string({require: true})
+        },
+        resolve: () => {
+            return [new DataChannel("testorg", "testname", "testend")]
+        }
     }),
+    dataChannel: t.field({
+        type: DataChannel,
+        args: {
+            organization: t.arg.string({require: true}),
+            name: t.arg.string({require: true})
+        },
+        resolve: (root, args, context) => {
+            return new DataChannel("testorg", "testname", "testend")
+        }
+    })
   }),
 });
+
+builder.mutationType({
+    fields: (t) => ({
+        upsertDataChannel: t.field({
+            type: DataChannel,
+            args: {
+                organization: t.arg.string({require: true}),
+                name: t.arg.string({require: true}),
+                endpoint: t.arg.string({require: true})
+            },
+            resolve: (root, {organization, name, endpoint}, context) => {
+                console.log(`upserting ${organization}/${name}@${endpoint}`)
+
+                return new DataChannel(organization as string, name as string, endpoint as string)
+            }
+        }),
+        deleteDataChannel: t.field({
+            type: DataChannel,
+            args: {
+                organization: t.arg.string({require: true}),
+                name: t.arg.string({require: true}),
+            },
+            resolve: (root, {organization, name}, context) => {
+                return new DataChannel(organization as string, name as string, "endpoint")
+            }
+        })
+    })
+})
 
 export const schema = builder.toSchema()
