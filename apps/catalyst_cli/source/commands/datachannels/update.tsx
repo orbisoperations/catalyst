f0255ff9faf13c77eea55815986f4c467dd88409
j.taylor <j.taylor@orbisops.com>
2024-02-07 05:53:45 +0000

    integrating cli with api

--- apps/catalyst_cli/source/commands/datachannels/update.tsx
+++ apps/catalyst_cli/source/commands/datachannels/update.tsx
@@ -2,6 +2,21 @@ import React from 'react';
 import {Text} from 'ink';
 import zod from 'zod';
 
+import { GraphQLClient } from 'graphql-request'
+import { getConfig } from '../../config.js'
+
+function getGraphqlClient(): [GraphQLClient, React.JSX.Element] {
+    // @ts-ignore
+    const [config, configLogs] = getConfig();
+
+    const client = new GraphQLClient(config.catalystRegistrarEndpoint)
+
+    return [client, (<>
+    {configLogs}
+    <Text>Created GraphqlClient for endpoint {config.catalystRegistrarEndpoint}</Text>
+    </>)]
+}
+
 export const args = zod.tuple([zod.string().describe("organization name"), zod.string().describe("data channel name"), zod.string().describe("endpoint")]).describe("[organizaiton name] [channel name] [endpoint]")
 
 type Props = {
