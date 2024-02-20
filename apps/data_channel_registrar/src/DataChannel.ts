d9ccecdb5c00b342b9bd6e09b52e4aa083e678bf
Bert Stover <b.stover@orbisops.com>
2024-02-20 15:36:14 -0500

    mid DO commit

--- apps/data_channel_registrar/src/DataChannel.ts
+++ apps/data_channel_registrar/src/DataChannel.ts
@@ -1,5 +1,6 @@
 import { builder } from './builder'
 import { prisma } from './db'
+import { Registrar } from './Registrar'
 
 export class DataChannel {
     organization: string
@@ -15,96 +16,96 @@ export class DataChannel {
 
 let d0Stub: DataChannel[] = [] as Array<DataChannel>
 
-builder.prismaObject('DataChannel',{
-    fields: (t) => ({
-        organization: t.exposeString('organization'),
-        name: t.exposeString('name'),
-        endpoint: t.exposeString('endpoint')
-    }),
-})
-builder.queryField('listDataChannels', (t) =>
-t.prismaField({
-    type: ['DataChannel'],
-    args: {
-        name: t.arg.string({required: false}),
-        organization: t.arg.string({required: false}),
-    },
-    resolve: async (query, root, args, ctx, info) => {
-        return prisma.dataChannel.findMany({...query})
-    }
-}
-)
-);
-builder.queryField('getDataChannel', (t) => t.prismaField({
-        type: 'DataChannel',
-        nullable: true,
-        args: {
-            organization: t.arg.string({required: true}),
-            name: t.arg.string({required: true})
-        },
-        resolve: (query,root, args, context) => {
-            const filtered = d0Stub.filter(item => {
-                if (item.organization == args.organization && item.name == args.name) {
-                    return true
-                }
-
-                return false
-            })
-
-            if (filtered.length == 1) {
-                return filtered[0]
-            }
-
-            return  undefined
-
-        }
+    builder.prismaObject('DataChannel', {
+        fields: (t) => ({
+            organization: t.exposeString('organization'),
+            name: t.exposeString('name'),
+            endpoint: t.exposeString('endpoint')
+        }),
     })
-);
-
-
-builder.mutationField(
-        'upsertDataChannel', (t)=>t.prismaField({
-            type: 'DataChannel',
-            args: {
-                organization: t.arg.string({required: true}),
-                name: t.arg.string({required: true}),
-                endpoint: t.arg.string({required: true})
-            },
-            resolve: (query,root, args, context) => {
-                console.log(`upserting ${args.organization}/${args.name}@${args.endpoint}`)
-                const dc = new DataChannel(args.organization as string, args.name as string, args.endpoint as string)
-
-                d0Stub.push(dc)
-
-                return dc
+    builder.queryField('listDataChannels', (t) =>
+        t.prismaField({
+                type: ['DataChannel'],
+                args: {
+                    name: t.arg.string({required: false}),
+                    organization: t.arg.string({required: false}),
+                },
+                resolve: async (query, root, args, ctx, info) => {
+                    return prisma.dataChannel.findMany({...query})
+                }
             }
-        }
         )
-);
-builder.mutationField(
-        'deleteDataChannel', (t)=> t.prismaField({
+    );
+    builder.queryField('getDataChannel', (t) => t.prismaField({
             type: 'DataChannel',
             nullable: true,
             args: {
                 organization: t.arg.string({required: true}),
-                name: t.arg.string({required: true}),
+                name: t.arg.string({required: true})
             },
             resolve: (query, root, args, context) => {
-                let deleted: DataChannel | undefined = undefined;
-                const filtered: DataChannel[] = []
-                for (const e of d0Stub) {
-                    if (e.organization == args.organization && e.name) {
-                        deleted = e;
-                    } else {
-                        filtered.push(e);
+                const filtered = d0Stub.filter(item => {
+                    if (item.organization == args.organization && item.name == args.name) {
+                        return true
                     }
+
+                    return false
+                })
+
+                if (filtered.length == 1) {
+                    return filtered[0]
                 }
 
-                console.log("new set of dcs: ", d0Stub, filtered)
-                d0Stub = filtered
+                return undefined
 
-                return deleted
             }
-        }
+        })
+    );
+
+
+    builder.mutationField(
+        'upsertDataChannel', (t) => t.prismaField({
+                type: 'DataChannel',
+                args: {
+                    organization: t.arg.string({required: true}),
+                    name: t.arg.string({required: true}),
+                    endpoint: t.arg.string({required: true})
+                },
+                resolve: (query, root, args, context) => {
+                    console.log(`upserting ${args.organization}/${args.name}@${args.endpoint}`)
+                    const dc = new DataChannel(args.organization as string, args.name as string, args.endpoint as string)
+
+                    d0Stub.push(dc)
+
+                    return dc
+                }
+            }
         )
-);
+    );
+    builder.mutationField(
+        'deleteDataChannel', (t) => t.prismaField({
+                type: 'DataChannel',
+                nullable: true,
+                args: {
+                    organization: t.arg.string({required: true}),
+                    name: t.arg.string({required: true}),
+                },
+                resolve: (query, root, args, context) => {
+                    let deleted: DataChannel | undefined = undefined;
+                    const filtered: DataChannel[] = []
+                    for (const e of d0Stub) {
+                        if (e.organization == args.organization && e.name) {
+                            deleted = e;
+                        } else {
+                            filtered.push(e);
+                        }
+                    }
+
+                    console.log("new set of dcs: ", d0Stub, filtered)
+                    d0Stub = filtered
+
+                    return deleted
+                }
+            }
+        )
+    );
