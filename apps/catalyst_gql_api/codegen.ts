8ce45c6a840a3f9df819141c77f7c8257a6ce67c
Geoff Seemueller <100710144+seegeoff@users.noreply.github.com>
2024-03-21 15:25:51 -0400

    03 20 changed from by name to by id query for data channel and specified creator organization (#61)
    * adds kysely/prisma
    
    * wip
    
    i# Please enter the commit message for your changes. Lines starting
    
    * Updated DB migrations
    
    * Added a create mutation, tests, and graphql code generation.
    
    * Changed from byName to byId query for Data Channel and specified creatorOrganization.
    
    * Added an update mutation and test
    
    * Added a delete DataChannel mutation with test.
    
    * Delete packages/schema/copyMigrations.bun.js
    
    ---------
    
    Co-authored-by: Bert Stover <b.stover@orbisops.com>

---
+++ apps/catalyst_gql_api/codegen.ts
@@ -0,0 +1,19 @@
+import type { CodegenConfig } from '@graphql-codegen/cli';
+import { printSchema } from 'graphql';
+import  schema  from './src/pothos/schemaBuilder';
+
+const config: CodegenConfig = {
+    schema: printSchema(schema),
+    // documents: ['src/**/*.tsx'],
+    generates: {
+        './src/gql/': {
+            preset: 'client',
+            plugins: [],
+        },
+        'schema.graphql': {
+            plugins: ['schema-ast'],
+        },
+    },
+};
+
+export default config;
---
+++ apps/catalyst_gql_api/src/worker.ts
@@ -0,0 +1,34 @@
+import {Hono} from "hono";
+import {createYoga} from "graphql-yoga";
+import schemaBuilder from "./pothos/schemaBuilder";
+import {CatalystKyselySchema, CatalystKyselyTypes} from "@catalyst/schema";
+import {Kysely} from "kysely";
+import {D1Dialect} from "kysely-d1";
+const app = new Hono<{ Bindings: Env }>();
+
+export type Env = & Record<string, string> & {
+  APP_DB: D1Database;
+};
+
+
+app.use("/data-channels/graphql", async (c) => {
+
+
+  const db = new Kysely<CatalystKyselySchema>({
+    dialect: new D1Dialect({database: c.env.APP_DB})
+  });
+
+
+  const yoga = createYoga({
+    schema: schemaBuilder,
+    context: {
+      ...c,
+      db: db,
+    },
+  });
+  return yoga.handle(c.req.raw as Request);
+});
+
+
+
+export default app;
