08969d88fbd4cd494ba36d242d0bf04267cd4aec
Geoff Seemueller <g.seemueller@orbisops.com>
2024-03-21 16:58:10 -0400

    formatted with prettier

--- apps/data_channel_registrar/codegen.ts
+++ apps/data_channel_registrar/codegen.ts
@@ -1,19 +1,18 @@
 import type { CodegenConfig } from '@graphql-codegen/cli';
 import { printSchema } from 'graphql';
-import  schema  from './src/pothos/schemaBuilder';
+import schema from './src/pothos/schemaBuilder';
 
 const config: CodegenConfig = {
-    schema: printSchema(schema),
-    // documents: ['src/**/*.tsx'],
-    generates: {
-        './generated/gql/': {
-            preset: 'client',
-            plugins: [],
-        },
-        'schema.graphql': {
-            plugins: ['schema-ast'],
-        },
+  schema: printSchema(schema),
+  generates: {
+    './generated/gql/': {
+      preset: 'client',
+      plugins: [],
     },
+    'schema.graphql': {
+      plugins: ['schema-ast'],
+    },
+  },
 };
 
 export default config;
