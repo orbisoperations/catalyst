8a780d47757cdc167f6dd2ef7aa454580e51a905
Geoff Seemueller <100710144+seegeoff@users.noreply.github.com>
2024-03-28 11:13:15 -0400

    Integrate data channel gateway + registrar (#68)

--- apps/data_channel_registrar/src/worker.ts
+++ apps/data_channel_registrar/src/worker.ts
@@ -1,28 +1,30 @@
-import { Hono } from 'hono';
-import { createYoga } from 'graphql-yoga';
+import {Hono} from 'hono';
+import {createYoga} from 'graphql-yoga';
 import schemaBuilder from './pothos/schemaBuilder';
-import { CatalystKyselySchema } from '@catalyst/schema';
-import { Kysely } from 'kysely';
-import { D1Dialect } from 'kysely-d1';
+import {CatalystKyselySchema} from '@catalyst/schema';
+import {Kysely} from 'kysely';
+import {D1Dialect} from 'kysely-d1';
+
 const app = new Hono<{ Bindings: Env }>();
 
 export type Env = Record<string, string> & {
   APP_DB: D1Database;
 };
 
-app.use('/data-channels/graphql', async c => {
+app.use('/graphql', async ctx => {
+
   const db = new Kysely<CatalystKyselySchema>({
-    dialect: new D1Dialect({ database: c.env.APP_DB }),
+    dialect: new D1Dialect({database: ctx.env.APP_DB}),
   });
 
   const yoga = createYoga({
     schema: schemaBuilder,
-    context: {
-      ...c,
-      db: db,
-    },
+    graphqlEndpoint: '/graphql',
   });
-  return yoga.handle(c.req.raw as Request);
+
+  return yoga(ctx.req.raw, {
+    db: db,
+  });
 });
 
 export default app;
