448dd901c603929ac36fdddf9a077796a5c76253
Jesse Taylor <j.taylor@orbisops.com>
2024-03-29 12:55:23 -0700

    moved jwt verification to library to simplify implementation

---
+++ packages/errors/index.ts
@@ -0,0 +1,16 @@
+
+type Status = 200 | 400 | 401 | 403 | 500 | 501
+export class CError extends Error {
+    status: Status
+
+    constructor(message: string, status: Status) {
+        super(message);
+        this.status = status
+    }
+
+    toJSON(): [Object, Status] {
+        return [{
+            error: this.message
+        }, this.status]
+    }
+}
