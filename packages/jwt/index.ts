0bc81fb363c9c384775906ecdf9896b31c8de29e
Jesse Taylor <j.taylor@orbisops.com>
2024-03-29 12:55:23 -0700

    moved jwt verification to library to simplify implementation

--- packages/jwt/index.ts
+++ packages/jwt/index.ts
@@ -3,6 +3,8 @@ import {KeyLike, importSPKI, jwtVerify, JWTPayload} from "jose";
 
 import {Client, fetchExchange, gql} from '@urql/core';
 
+import {StatusCode} from "hono/utils/http-status"
+
 export class UrlqGraphqlClient {
 
     client: Client;
@@ -35,7 +37,7 @@ export class VerifyingClient {
         this.endpoint = publicKeyEndpoint;
     }
 
-    async verify(token: string): Promise<JWTPayload | undefined> {
+    async verify(token: string, issuer: string, claims: string[] ): Promise<[boolean, {msg: string, status: StatusCode}?]> {
         if (!this.publicKey) {
             const client = new UrlqGraphqlClient(this.endpoint)
             const pubKey = await client.getPublickKey()
@@ -44,12 +46,58 @@ export class VerifyingClient {
         }
 
         console.log("verifying jwt");
+        var jwtClaims: JWTPayload
         try {
-            const {payload, protectedHeader} = await jwtVerify(token, this.publicKey);
-            return payload
+            const { payload} = await jwtVerify(token, this.publicKey);
+            jwtClaims = payload
         } catch (e) {
             console.error(e)
-            return undefined
+            return [false, {msg: "JWT Invalid", status: 401}]
         }
+
+        /// check that the issuer is good
+        if (jwtClaims.iss !== issuer) {
+            console.log("jwt issuer is bunk")
+            return [false, {msg: "JWT Issuer Invalid", status: 401}]
+        }
+        // check that claims exist, non-exists is falsey, empty array can be true
+        if (!("claims" in jwtClaims)) {
+            console.log("jwt claims non-existent")
+            return [false, {msg: "JWT Claims Missing", status: 401}]
+        }
+
+        const dataChannelClaims: string[] = jwtClaims["claims"] as string[]
+        // check that our claims are in the claims
+        if (claims.filter(e => dataChannelClaims.includes(e)).length != claims.length) {
+            return [false, {msg: "JWT Claims Do Not Align", status: 401}]
+        }
+
+        return [true]
     }
+}
+
+type Token = string
+export type JWTError = {
+    msg: string
+    status: StatusCode
+}
+
+export function GradTokenInHeader(authHeader: string | undefined): Token | JWTError {
+    // authheader should be in format "Bearer tokenstring"
+    if (!authHeader) {
+        return {
+                msg: "No Credenetials Supplied",
+                status: 400
+            }
+    }
+
+    const  headerElems = authHeader.split(" ")
+    if (headerElems.length != 2) {
+        return {
+                msg: "No Credenetials Supplied",
+                status: 400
+            }
+    }
+
+    return headerElems[1]
 }
