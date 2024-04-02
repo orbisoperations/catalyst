a9f9a891118f9081ce55550546eb96d0c607e140
mfromoops <101655499+mfromoops@users.noreply.github.com>
2024-04-02 19:15:26 -0400

    Delete and Update Data Channels (#78)

--- apps/catalyst-ui/app/channels/[id]/page.tsx
+++ apps/catalyst-ui/app/channels/[id]/page.tsx
@@ -1,8 +1,14 @@
 "use client";
 export const runtime = "edge";
-import { OrbisBadge, ShareButton, TrashButton } from "@/components/elements";
+import {
+  OrbisBadge,
+  OrbisButton,
+  ShareButton,
+  TrashButton,
+} from "@/components/elements";
 import { DetailedView } from "@/components/layouts";
 import { navigationItems } from "@/utils/nav.utils";
+import { gql, useMutation, useQuery } from "@apollo/client";
 import { Card, CardBody, CardHeader } from "@chakra-ui/card";
 import {
   Box,
@@ -13,11 +19,43 @@ import {
   StackDivider,
   Text,
 } from "@chakra-ui/layout";
-import { FormControl, Input, Switch } from "@chakra-ui/react";
-import { useState } from "react";
+import {
+  FormControl,
+  Input,
+  Modal,
+  ModalBody,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  Switch,
+  Textarea,
+  useDisclosure,
+} from "@chakra-ui/react";
+import { PencilSquareIcon } from "@heroicons/react/20/solid";
+import { useParams, useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
 
+type DataChannel = {
+  id: string;
+  name: string;
+  description: string;
+  endpoint: string;
+  creatorOrganization: string;
+};
 export default function DataChannelDetailsPage() {
-  const [isOwner, setIsOwner] = useState(true);
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const editDisclosure = useDisclosure();
+  const DELETE_QUERY = gql`
+    mutation deleteChannel($id: String!) {
+      deleteDataChannel(id: $id)
+    }
+  `;
+  const router = useRouter();
+  const { id } = useParams();
+  const [deleteChannel] = useMutation(DELETE_QUERY);
+  const [channel, setChannel] = useState<DataChannel>();
+  const [editChannel, setEditChannel] = useState<DataChannel>();
   const organizations = [
     {
       name: "Organization 1",
@@ -36,44 +74,230 @@ export default function DataChannelDetailsPage() {
       id: "org4",
     },
   ];
+  const GET_DATA_CHANNEL = gql`
+    query getDataChannel($id: String!) {
+      dataChannelById(id: $id) {
+        id
+        name
+        description
+        endpoint
+        creatorOrganization
+      }
+    }
+  `;
+  const UPDATE_DATA_CHANNEL = gql`
+    mutation updateDataChannel(
+      $id: String!
+      $name: String!
+      $description: String!
+      $endpoint: String!
+      $creatorOrganization: String!
+    ) {
+      updateDataChannel(
+        input: {
+          id: $id
+          name: $name
+          description: $description
+          endpoint: $endpoint
+          creatorOrganization: $creatorOrganization
+        }
+      ) {
+        id
+      }
+    }
+  `;
+
+  const [updateDataChannel] = useMutation(UPDATE_DATA_CHANNEL);
+
+  const { data, loading, error, refetch } = useQuery(GET_DATA_CHANNEL, {
+    variables: { id },
+  });
+
+  useEffect(() => {
+    if (data && data.dataChannelById) {
+      setChannel(data.dataChannelById);
+      setEditChannel({ ...data.dataChannelById });
+    }
+  }, [loading]);
   return (
     <DetailedView
       actions={
-        isOwner ? (
-          <Flex gap={10}>
-            <Flex gap={2} align={"center"}>
-              <Switch colorScheme="green" />
-              <Text>Enable</Text>
-            </Flex>
-            <TrashButton />
+        <Flex gap={10}>
+          <Flex gap={2} align={"center"}>
+            <Switch colorScheme="green" />
+            <Text>Enable</Text>
           </Flex>
-        ) : (
-          <></>
-        )
+          <Flex gap={5} align={"center"}>
+            <OrbisButton p={2} rounded={"full"} onClick={editDisclosure.onOpen}>
+              <PencilSquareIcon />
+            </OrbisButton>
+            <TrashButton onClick={onOpen} />
+          </Flex>
+        </Flex>
       }
       headerTitle={{
-        adjacent: !isOwner ? <OrbisBadge> Shared with you </OrbisBadge> : <></>,
-        text: "N Metadata",
+        adjacent:
+          channel?.creatorOrganization === "org1" ? (
+            <OrbisBadge> Shared with you </OrbisBadge>
+          ) : (
+            <></>
+          ),
+        text: "Channel: " + (channel ? channel.name : "Loading..."),
       }}
-      subtitle="Description for the data channel"
+      subtitle={channel?.description ?? ""}
       topbaractions={navigationItems}
       topbartitle="Data Channel Details"
     >
       <div>
-        <Flex gap={2} mb={5} align={"center"}>
-          <Switch
-            colorScheme="green"
-            id="channel-status"
-            size="md"
-            isChecked={isOwner ? true : false}
-            onChange={(e) => {
-              setIsOwner((prev) => {
-                return e.target.checked;
-              });
-            }}
-          />
-          <Text> Is Owner (tmp control)</Text>
-        </Flex>
+        <div id="delete-modal">
+          <Modal isOpen={isOpen} onClose={onClose}>
+            <ModalOverlay />
+            <ModalContent>
+              <ModalHeader>
+                Are you sure you want to delete this channel?
+              </ModalHeader>
+              <ModalBody>
+                <Text>
+                  Deleting this channel will remove all associated data
+                </Text>
+              </ModalBody>
+              <ModalFooter>
+                <Flex gap={5}>
+                  <OrbisButton colorScheme="gray" onClick={onClose}>
+                    Cancel
+                  </OrbisButton>
+                  <OrbisButton
+                    colorScheme="red"
+                    onClick={() => {
+                      if (id && typeof id === "string")
+                        deleteChannel({ variables: { id } }).then(() => {
+                          onClose();
+                          router.push("/channels");
+                        });
+                    }}
+                  >
+                    Delete
+                  </OrbisButton>
+                </Flex>
+              </ModalFooter>
+            </ModalContent>
+          </Modal>
+        </div>
+        <div id="edit-modal">
+          <Modal
+            isOpen={editDisclosure.isOpen}
+            onClose={editDisclosure.onClose}
+          >
+            <ModalOverlay />
+            <ModalContent>
+              <ModalHeader>Edit Data Channel</ModalHeader>
+              <ModalBody>
+                <form
+                  onSubmit={(e) => {
+                    e.preventDefault();
+                    const formData = new FormData(e.currentTarget);
+                    const data = {
+                      name: formData.get("name"),
+                      description: formData.get("description"),
+                      endpoint: formData.get("endpoint"),
+                      creatorOrganization: formData.get("organization"),
+                    };
+                    if (channel) {
+                      const variables = {
+                        id: channel.id,
+                        ...data,
+                      };
+                      updateDataChannel({
+                        variables,
+                      }).then(() => {
+                        refetch().then((res) => {
+                          setChannel(res.data.dataChannelById);
+                          setEditChannel(res.data.dataChannelById);
+                        });
+                        editDisclosure.onClose();
+                      });
+                    }
+                  }}
+                >
+                  <Grid gap={5}>
+                    <FormControl display={"grid"} gap={2}>
+                      <label htmlFor="name">Data Channel Name</label>
+                      <Input
+                        rounded="md"
+                        name="name"
+                        required={true}
+                        value={editChannel?.name}
+                        onChange={(e) => {
+                          editChannel &&
+                            setEditChannel({
+                              ...editChannel,
+                              name: e.target.value,
+                            });
+                        }}
+                        placeholder="Data Channel Name"
+                      />
+                    </FormControl>
+                    <FormControl display={"grid"} gap={2}>
+                      <label htmlFor="description">Description</label>
+                      <Textarea
+                        rounded="md"
+                        name="description"
+                        required={true}
+                        value={editChannel?.description}
+                        onChange={(e) => {
+                          editChannel &&
+                            setEditChannel({
+                              ...editChannel,
+                              description: e.target.value,
+                            });
+                        }}
+                        placeholder="Description"
+                      />
+                    </FormControl>
+                    <FormControl display={"grid"} gap={2}>
+                      <label htmlFor="endpoint">Endpoint URL</label>
+                      <Input
+                        rounded="md"
+                        name="endpoint"
+                        required={true}
+                        value={editChannel?.endpoint}
+                        onChange={(e) => {
+                          editChannel &&
+                            setEditChannel({
+                              ...editChannel,
+                              endpoint: e.target.value,
+                            });
+                        }}
+                        placeholder="Endpoint URL"
+                      />
+                    </FormControl>
+                    <FormControl display={"none"}>
+                      <label htmlFor="organization"></label>
+                      <Input
+                        rounded="md"
+                        name="organization"
+                        required={true}
+                        value={"org1"}
+                      />
+                    </FormControl>
+                    <Flex justifyContent={"space-between"}>
+                      <OrbisButton
+                        colorScheme="gray"
+                        onClick={() => {
+                          editDisclosure.onClose();
+                          setEditChannel(channel);
+                        }}
+                      >
+                        Cancel
+                      </OrbisButton>
+                      <OrbisButton type="submit">Save</OrbisButton>
+                    </Flex>
+                  </Grid>
+                </form>
+              </ModalBody>
+            </ModalContent>
+          </Modal>
+        </div>
         <form>
           <Grid gap={5}>
             <FormControl display={"grid"} gap={2}>
@@ -82,6 +306,8 @@ export default function DataChannelDetailsPage() {
                 rounded="md"
                 name="endpoint"
                 required={true}
+                readOnly={true}
+                value={channel?.endpoint ?? ""}
                 placeholder="Endpoint URL"
               />
             </FormControl>
@@ -113,7 +339,7 @@ export default function DataChannelDetailsPage() {
                   </Stack>
                 </CardBody>
               </Card>
-              {isOwner && (
+              {channel?.creatorOrganization === "org2" && (
                 <Card>
                   <CardHeader>
                     <Flex justify={"space-between"} gap={5} align={"center"}>
--- apps/catalyst-ui/app/channels/page.tsx
+++ apps/catalyst-ui/app/channels/page.tsx
@@ -8,72 +8,83 @@ import {
 import { ListView } from "@/components/layouts";
 import { navigationItems } from "@/utils/nav.utils";
 import { Flex } from "@chakra-ui/layout";
-import { Card } from "@chakra-ui/react";
+import { Card, CardBody } from "@chakra-ui/react";
 import { useRouter } from "next/navigation";
-import { useQuery, gql, OperationVariables} from "@apollo/client";
-
-
+import { useQuery, gql, OperationVariables } from "@apollo/client";
+import { useEffect } from "react";
 export default function DataChannelListPage() {
-    const router = useRouter();
-    const GET_DATA_CHANNEL_BY_CREATOR_ORG = gql`
-        query dataChannelsByCreatorOrg (
-            $creatorOrganization: String!
-        ){
-            dataChannelsByCreatorOrg(
-                creatorOrganization: $creatorOrganization
-            )
-            {
-                description
-                endpoint
-                id
-                name
-                creatorOrganization
-            }
-        }`
-    // TODO: Update to use the dynamic organization id
-    const orgVariable: OperationVariables = {creatorOrganization: "org1"};
-    const { data ,loading, error} = useQuery(GET_DATA_CHANNEL_BY_CREATOR_ORG, {
-        variables: orgVariable
-    });
+  const router = useRouter();
+  const GET_DATA_CHANNEL_BY_CREATOR_ORG = gql`
+    query dataChannelsByCreatorOrg($creatorOrganization: String!) {
+      dataChannelsByCreatorOrg(creatorOrganization: $creatorOrganization) {
+        description
+        endpoint
+        id
+        name
+        creatorOrganization
+      }
+    }
+  `;
+  // TODO: Update to use the dynamic organization id
+  const orgVariable: OperationVariables = { creatorOrganization: "org1" };
+  const { data, loading, error } = useQuery(GET_DATA_CHANNEL_BY_CREATOR_ORG, {
+    variables: orgVariable,
+  });
 
-    return (
-        <ListView
-            actions={
-                <Flex gap={5}>
-                    <CreateButton
-                        onClick={() => {
-                            router.push("/channels/create");
-                        }}
-                    />
-                </Flex>
-            }
-            topbaractions={navigationItems}
-            headerTitle={{
-                adjacent: <OrbisBadge>Hello</OrbisBadge>,
-                text: "Data Channels",
+  useEffect(() => {
+    console.log(loading, data);
+  }, [loading]);
+
+  return (
+    <ListView
+      actions={
+        <Flex gap={5}>
+          <CreateButton
+            onClick={() => {
+              router.push("/channels/create");
             }}
-            positionChildren="bottom"
-            subtitle="All your data channels in one place."
-            table={
-                <Card variant={"outline"} shadow={"md"}>
-                    { data &&  <OrbisTable
-                        headers={["Data Channel", "Description", "Endpoint"]}
-                        rows={
-                            data.dataChannelsByCreatorOrg.map((channel: { id: string; name: string; description: string; endpoint: string; }) => {
+          />
+        </Flex>
+      }
+      topbaractions={navigationItems}
+      headerTitle={{
+        adjacent: <OrbisBadge>Hello</OrbisBadge>,
+        text: "Data Channels",
+      }}
+      positionChildren="bottom"
+      subtitle="All your data channels in one place."
+      table={
+        <Card variant={"outline"} shadow={"md"}>
+          {data && data.dataChannelsByCreatorOrg.length > 0 ? (
+            <OrbisTable
+              headers={["Data Channel", "Description", "Endpoint"]}
+              rows={data.dataChannelsByCreatorOrg.map(
+                (channel: {
+                  id: string;
+                  name: string;
+                  description: string;
+                  endpoint: string;
+                }) => {
                   return [
-                      <Flex key={"1"} justifyContent={"space-between"}>
-                          <OpenButton
-                              onClick={() => router.push("/channels/"+ channel.id)}
-                          >
-                                 {channel.name}
-                          </OpenButton>
-                          <OrbisBadge>Shared</OrbisBadge>
-                      </Flex>,
-                       channel.description, channel.endpoint,
-                  ]
-
-              })}
-          /> }
+                    <Flex key={"1"} justifyContent={"space-between"}>
+                      <OpenButton
+                        onClick={() => router.push("/channels/" + channel.id)}
+                      >
+                        {channel.name}
+                      </OpenButton>
+                      <OrbisBadge>Shared</OrbisBadge>
+                    </Flex>,
+                    channel.description,
+                    channel.endpoint,
+                  ];
+                }
+              )}
+            />
+          ) : (
+            <CardBody>
+              {loading ? "Loading..." : "No data channels found"}
+            </CardBody>
+          )}
         </Card>
       }
       topbartitle="Data Channels"
