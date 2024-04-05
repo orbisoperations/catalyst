7fcf104b5faf505253ba22272211044e20721b0e
Geoff Seemueller <g.seemueller@orbisops.com>
2024-04-05 13:48:56 -0400

    Gateway has a test that asserts channels with no claims

--- apps/data_channel_gateway/tests/index.spec.ts
+++ apps/data_channel_gateway/tests/index.spec.ts
@@ -1,91 +1,97 @@
 // test/index.spec.ts
-import { env, SELF } from "cloudflare:test";
-import { describe, it, expect } from "vitest";
-import worker from "../src/index";
-import {createRequest} from "@urql/core";
+import {env, SELF} from "cloudflare:test";
+import {describe, it, expect} from "vitest";
 
-// For now, you'll need to do something like this to get a correctly-typed
-// `Request` to pass to `worker.fetch()`.
-// @ts-ignore
-const IncomingRequest = Request<unknown, IncomingRequestCfProperties>;
 
 describe("gateway jwt validation", () => {
-
-  // it("returns gf'd for a invalid token", async () => {
-  //   const badToken = 'fake-and-insecure';
-  //
-  //   const headers = new Headers();
-  //   headers.set('Authorization', `Bearer ${badToken}`)
-  //
-  //
-  //   const response = await SELF.fetch('https://data-channel-gateway/graphql', {
-  //     method: 'GET',
-  //     headers
-  //   });
-  //
-  //   expect(await response.text()).toMatchInlineSnapshot('"Token validation failed"');
-  // });
-
-  // it("returns GF'd for no auth header", async () => {
-  //   const response = await SELF.fetch('https://data-channel-gateway/graphql', {
-  //     method: 'GET',
-  //   });
-  //
-  //   expect(await response.text()).toMatchInlineSnapshot(`"{"error":"No Credenetials Supplied"}"`);
-  // });
-
-
-  it("works with a known good token", async () => {
-
-    const getToken = async (claims: any) => {
-      const tokenQuery = `
+  const getToken = async (claims: any) => {
+    const tokenQuery = `
       mutation {
         sign(entity: "test-entity", claims: [])
       }
     `
 
-      const response =  await  env.AUTHX_TOKEN_API.fetch('https://authx-token-api/graphql', {
-        method: "POST",
-        body: JSON.stringify({
-          query: tokenQuery
-        }),
-        headers: {
-          'content-type': 'application/json'
-        },
-      })
+    const response = await env.AUTHX_TOKEN_API.fetch('https://authx-token-api/graphql', {
+      method: "POST",
+      body: JSON.stringify({
+        query: tokenQuery
+      }),
+      headers: {
+        'content-type': 'application/json'
+      },
+    })
 
-      const {data} =  await response.json() as {
-        data: { sign: string }
-      };
-
-      return data.sign;
+    const {data} = await response.json() as {
+      data: { sign: string }
     };
 
-    const token = await getToken([]);
+    return data.sign;
+  };
 
+  it("returns gf'd for a invalid token", async () => {
+    const badToken = 'fake-and-insecure';
+
+    const headers = new Headers();
+    headers.set('Authorization', `Bearer ${badToken}`)
 
 
     const response = await SELF.fetch('https://data-channel-gateway/graphql', {
+      method: 'GET',
+      headers
+    });
+
+    expect(await response.text()).toMatchInlineSnapshot('"Token validation failed"');
+  });
+
+  it("returns GF'd for no auth header", async () => {
+    const response = await SELF.fetch('https://data-channel-gateway/graphql', {
+      method: 'GET',
+    });
+
+    expect(await response.text()).toMatchInlineSnapshot(`"{"error":"No Credenetials Supplied"}"`);
+  });
+
+
+  it("works with a known good token no claims", async () => {
+    const token = await getToken([]);
+    const response = await SELF.fetch('https://data-channel-gateway/graphql', {
       method: 'POST',
       headers: {
-        'Authorization': `Bearer ${token}`
+        'Authorization': `Bearer ${token}`,
+        'content-type': 'application/json',
+        'Accepts': 'application/json'
       },
       body: JSON.stringify({
-        query: `
-          query {
-            allDataChannels {
-              id
-            }
-          }
-        `
+        query: `{
+            __type(name: "Query") {
+                name
+                fields {
+                  name
+                  type {
+                    name
+                    kind
+                    ofType {
+                      name
+                      kind
+                    }
+                  }
+                }
+              }
+          }`
       })
     });
 
-    console.log(response)
-    const responseText = await response.text()
+    const responsePayload = await response.json<{
+      data: {
+        __type: {
+          name: string;
+          fields: unknown[]
+        }}
+    }>();
 
-    console.log({responseText});
-
-    expect(responseText).toMatchInlineSnapshot("");
+    // Since we did not provide claims when the token was created, this will only return the health query in the list of fields
+    expect(responsePayload.data["__type"].fields).toHaveLength(1);
+    // @ts-ignore
+    expect(responsePayload.data["__type"].fields[0]['name']).toBe('health');
   });
 });
--- apps/data_channel_gateway/vitest.config.ts
+++ apps/data_channel_gateway/vitest.config.ts
@@ -1,4 +1,5 @@
-import {defineWorkersConfig, defineWorkersProject, readD1Migrations} from "@cloudflare/vitest-pool-workers/config";
+// @ts-ignore
+import {defineWorkersProject, readD1Migrations} from "@cloudflare/vitest-pool-workers/config";
 import path from "node:path";
 
 const authxServicePath = path.resolve("../authx_token_api/dist/index.js");
