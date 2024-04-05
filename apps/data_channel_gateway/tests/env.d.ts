9ba4e9fc2ecad0c960d44b1237e53d55a5234f8f
Geoff Seemueller <g.seemueller@orbisops.com>
2024-04-05 15:35:43 -0400

    Added Seed data via specific migration pattern

--- apps/data_channel_gateway/tests/env.d.ts
+++ apps/data_channel_gateway/tests/env.d.ts
@@ -2,7 +2,8 @@ declare module "cloudflare:test" {
   // Controls the type of `import("cloudflare:test").env`
 
   interface ProvidedEnv extends Env {
-    TEST_MIGRATIONS: D1Migration[];
+    APP_MIGRATIONS: D1Migration[];
+    TEST_SEED_MIGRATIONS: D1Migration[];
     APP_DB: D1Database;
   }
 }
