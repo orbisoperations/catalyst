b412915166150671ffab9fc6792fd1fc3cd07b3c
Geoff Seemueller <g.seemueller@orbisops.com>
2024-04-10 13:03:23 -0400

    Add experimental-TAK server and datachannel_tak module
    This commit adds a new experimental server, "TAK", and introduces a new module called "datachannel_tak". It includes all the necessary files, from package JSON and configuration files, to the core JavaScript files for running the server and module. The TAK server uses the Docker system for creating and managing containers, which it pulls from a specific image. The Datachannel_tak module includes a manufacture data that interacts with GraphQL.

---
+++ examples/datachannel_tak/src/index.ts
@@ -0,0 +1,59 @@
+import { Hono } from 'hono'
+import {createYoga} from 'graphql-yoga';
+import { createSchema } from 'graphql-yoga'
+import { stitchingDirectives } from '@graphql-tools/stitching-directives';
+const { stitchingDirectivesTypeDefs, stitchingDirectivesValidator } = stitchingDirectives();
+
+const manufactures = [
+  {id: "1", name: "Tesla"},
+  {id: "2", name: "Rivian"},
+  {id: "3", name: "Saab"},
+  {id: "4", name: "Airbus"},
+  {id: "5", name: "Boeing"}
+];
+
+
+
+
+const typeDefs = `
+  ${stitchingDirectivesTypeDefs}
+    type Manufacture @canonical {
+      id: ID!
+      name: String
+    }
+  
+    type Query {
+      manufactures: [Manufacture]!
+      manufacture(id: ID!): Manufacture @merge(keyField: "id")
+      _sdl: String!
+    }
+  `
+
+export const schema = createSchema({
+  typeDefs: typeDefs ,
+  resolvers: {
+    Query: {
+      tak: () => {
+        // TODO: Add logic here to
+      },
+        _sdl: () => typeDefs,
+    }
+  }
+})
+
+const yoga = createYoga({
+  schema: schema,
+});
+
+const app = new Hono()
+
+app.get('/', (c) => {
+  return c.text('manufactures')
+})
+
+app.use("/graphql", async (c) => {
+  console.log(c.req.raw);
+  return yoga.handle(c.req.raw, c);
+});
+
+export default app
