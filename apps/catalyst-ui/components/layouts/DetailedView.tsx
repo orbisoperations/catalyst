36b8cc29a1f33834a89448ee5caf2f3604cda21a
mfromoops <101655499+mfromoops@users.noreply.github.com>
2024-04-11 17:23:31 -0400

    Loading states for channels list and detailed views (#85)

--- apps/catalyst-ui/components/layouts/DetailedView.tsx
+++ apps/catalyst-ui/components/layouts/DetailedView.tsx
@@ -1,11 +1,12 @@
 "use client";
-import { Box, Container, Flex, PropsOf, Text } from "@chakra-ui/react";
+import { Box, Container, Flex, PropsOf, Spinner, Text } from "@chakra-ui/react";
 import { DetailedHeader, Footer, TopBar } from "./components";
 import { HelpModal } from "../elements";
 
 type DetailedViewProps = PropsOf<typeof Box> & {
   headerTitle?: { text?: string; adjacent?: JSX.Element };
   actions?: JSX.Element | undefined;
+  showSpinner?: boolean;
   topbaractions?: { display: string; path: string }[];
   topbartitle?: string;
   subtitle?: string;
@@ -31,16 +32,22 @@ export const DetailedView = (props: DetailedViewProps) => {
         zIndex={10}
       />
       <Box height={"85%"} overflowY={"auto"} {...boxProps}>
-        <Container maxW="container.xl" p={2}>
-          <Box position={"relative"} p={5}>
-            <DetailedHeader
-              title={title}
-              actions={actions}
-              subtitle={subtitle}
-            />
-            <Box>{children}</Box>
-          </Box>
-        </Container>
+        {!props.showSpinner ? 
+          <Container maxW="container.xl" p={2}>
+            <Box position={"relative"} p={5}>
+              <DetailedHeader
+                title={title}
+                actions={actions}
+                subtitle={subtitle}
+              />
+              <Box>{children}</Box>
+            </Box>
+          </Container>
+          : 
+          <Flex height={'100%'}>
+            <Spinner size="xl" m={'auto'}/>
+          </Flex>
+        }
       </Box>
       <Footer>
         <Flex justify={"space-between"} w="100%" align={"center"}>
