27a8fdd5ced39ca3cf7d169fb6775f467bd32f4e
Geoff Seemueller <g.seemueller@orbisops.com>
2024-04-12 13:58:41 -0400

    Update dev port and add new test scheduling functionality in datachannel_tak
    The dev port in the datachannel_tak package has been updated. Also, new test scheduling functionality has been implemented in the index.ts file. Additionally, new dependencies have been added including "xml-js" and new files such as "hono.ts" and "run_as_scheduled.js" have been created to support these changes.

--- examples/datachannel_tak/src/poller.ts
+++ examples/datachannel_tak/src/poller.ts
@@ -1,44 +1,55 @@
-import { ApolloClient, InMemoryCache, gql } from '@apollo/client';
-
-// Create an Apollo client instance
-const client = new ApolloClient({
-  uri: 'https://your-graphql-endpoint.com/graphql',
-  cache: new InMemoryCache(),
-});
-
-getToken
-
-// Define the preset query
-const PRESET_QUERY = gql`
-    query {
-        airplanes {
-            id
-            mgrs
-        }
-        cars {
-            id
-            mgrs
-        }
-    }
-`;
-
-// Function to poll the GraphQL endpoint
-async function pollGraphQLEndpoint() {
-  try {
-    const result = await client.query({
-      query: PRESET_QUERY,
-      fetchPolicy: 'network-only',
-    });
-
-    // Process the result data
-    console.log('Polling result:', result.data);
-
-    // Schedule the next poll after a certain interval (e.g., 5 seconds)
-    setTimeout(pollGraphQLEndpoint, 5000);
-  } catch (error) {
-    console.error('Error polling GraphQL endpoint:', error);
-  }
-}
-
-// Start polling the GraphQL endpoint
-pollGraphQLEndpoint();
+// import {ApolloClient, InMemoryCache, gql, DocumentNode} from '@apollo/client';
+//
+// // Define the Poller class
+// export class Poller {
+//   private client: ApolloClient<unknown>;
+//   private static readonly PRESET_QUERY = gql`
+//       query {
+//           airplanes {
+//               id
+//               mgrs
+//           }
+//           cars {
+//               id
+//               mgrs
+//           }
+//       }
+//   `;
+//
+//   query: DocumentNode;
+//
+//   constructor(url: string, query?: DocumentNode) {
+//     this.client = new ApolloClient({
+//       uri: url,
+//       cache: new InMemoryCache(),
+//     });
+//
+//     this.query = query ?? Poller.PRESET_QUERY;
+//   }
+//
+//   async pollGraphQLEndpoint() {
+//     try {
+//       const result = await this.client.query({
+//         query: Poller.PRESET_QUERY,
+//         fetchPolicy: 'network-only',
+//       });
+//
+//       // Process the result data
+//       console.log('Polling result:', result.data);
+//
+//       // Schedule the next poll after a certain interval (e.g., 5 seconds)
+//       setTimeout(() => this.pollGraphQLEndpoint(), 5000);
+//     } catch (error) {
+//       console.error('Error polling GraphQL endpoint:', error);
+//     }
+//   }
+//
+//     // Interface to start polling the GraphQL endpoint
+//   startPolling() {
+//     this.pollGraphQLEndpoint();
+//   }
+// }
+//
+// // Create an instance of Poller and start polling
+// const poller = new Poller();
+// poller.startPolling();
