56d3f1acf5811a3d0ab99d7b3dfc270313ddd13e
Geoff Seemueller <g.seemueller@orbisops.com>
2024-04-15 13:33:59 -0400

    Remove old example datachannel
    This commit deletes all elements of the 'datachannel_tak_old' example, including related UI components, scripts, and dependencies. This is part of an overall effort to clean up and streamline the repository, removing outdated or unnecessary examples to improve clarity and maintainability.

--- examples/tak/src/lib/index.ts
+++ examples/tak/src/lib/index.ts
@@ -1,36 +1,82 @@
 import { connect } from 'cloudflare:sockets';
 import {generateLineCoordinates} from "./geoGen";
-import * as mgrs from "mgrs";
+import convert from "xml-js";
 
 export async function runTask(event: any, env: any, ctx: any) {
 
+		// TODO: Replace this exact function with a call to catalyst to retrieve data
 		function getTestData() {
-			return [...generateTestEntities(20)]
+			return [...generateTestEntities(100)]
 		}
 
 		const sendStuffToTak = async () => {
 			const data = await getTestData();
+
+			console.log({data});
 
 			const takAddr = { hostname: "localhost", port: 8999 };
 			const socket = connect(takAddr);
 			const writer = socket.writable.getWriter()
 			const encoder = new TextEncoder();
 
+			const now: string = new Date().toISOString();
 
+      let stale = new Date(now);
+      stale.setSeconds(stale.getSeconds() + 5);
+
+
+			// TODO: Evaluate values in the CoT XML for something more accurate. ce="45.3" hae="1-42.6" 1e="99.5"
+			// Documentation: https://www.mitre.org/sites/default/files/pdf/09_4937.pdf
 			const cotEvents = data.map(item => {
 				console.log(item);
-				let cotEvent = `
-<event version="2.0" uid="Marker" type="a-f-G-U-C" time="2024-06-15T19:22:35Z" start="2024-06-15T19:22:35Z" stale="2025-06-15T20:22:35Z" how="h-g-i-g-o">
-  <point mgrs="${item.mgrs}" hae="0" ce="9999999.0" le="9999999.0"/>
-  <detail>
-    <link uid="Platform" type="a-f-G-U-C" relation="p-p"/>
-    <__event>
-      <remarks>${item.name}</remarks>
-    </__event>
-  </detail>
-</event>
-`;
-				return cotEvent;
+
+				const now: string = new Date().toISOString();
+
+				console.log({now});
+
+				let stale = new Date(now);
+				stale.setSeconds(stale.getSeconds() + 5);
+
+				const cotEvent = {
+
+					event: {
+						_attributes: {
+							version: '2.0',
+							uid: item.id,
+							type: 'a-f-G-U-C',
+							time: now,
+							start: now,
+							stale: stale.toISOString()
+						},
+						point: {
+							_attributes: {
+								lat: item.lat,
+								lon: item.lon,
+								hae: '0',
+								ce: '9999999',
+								le: '9999999'
+							}
+						},
+						// detail: {
+						// 	contact: {
+						// 		_attributes: {
+						// 			callsign: 'Unit1'
+						// 		}
+						// 	},
+						// 	remarks: {
+						// 		_text: 'Example CoT event'
+						// 	}
+						// }
+					}
+				};
+
+
+				const options = {compact: true, ignoreComment: true, spaces: 4};
+
+				const xml = convert.js2xml(cotEvent, options);
+
+				console.log({xml});
+				return xml;
 			})
 
 			const futureTx = cotEvents.map(async (e) => {
@@ -49,14 +95,16 @@ export async function runTask(event: any, env: any, ctx: any) {
 function generateTestEntities(n: number) {
 	const testEntities = [];
 
-	const trackId = crypto.randomUUID();
-	const track = generateLineCoordinates("New York City", "Houston", 100)
+	const track = generateLineCoordinates("New York City", "Houston", n)
+
+	console.log({track});
 
 	for (let i = 0; i < n; i++) {
 		testEntities.push({
 			id: i + 1,
 			name: `Entity ${i + 1}`,
-			mgrs: mgrs.forward([track[0].lat, track[0].lng]),
+			lat: track[i].lat,
+			lon: track[i].lng,
 		});
 	}
 	return testEntities;
