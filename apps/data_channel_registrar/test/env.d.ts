3c995e1b9366c8a8eafa45e9bec40ae4892f7b8a
Jesse Taylor <j.taylor@orbisops.com>
2024-04-15 23:23:28 -0700

    rpc continued

--- apps/data_channel_registrar/test/env.d.ts
+++ apps/data_channel_registrar/test/env.d.ts
@@ -4,4 +4,8 @@ declare module "cloudflare:test" {
   interface ProvidedEnv extends Env{
     DO: DurableObjectNamespace
   }
+
+  // Ensure RPC properties and methods can be accessed with `SELF`
+  // @ts-ignore
+  export const SELF: Service<import("../src/worker").default>
 }
