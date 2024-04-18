481cca2fb727ca92127c15d95e016f78b5a813d4
Mario <mfromoops@gmail.com>
2024-04-18 16:28:54 -0400

    Initial integration of data channels with the ui

--- apps/catalyst-ui/env.d.ts
+++ apps/catalyst-ui/env.d.ts
@@ -1,3 +1,8 @@
+import { DataChannel } from "../../packages/schema_zod";
+import JWTWorker from "../authx_token_api/src";
+import RegistrarWorker from "../data_channel_registrar/src/worker";
+
 interface CloudflareEnv {
-    CATALYST_DATA_CHANNEL_REGISTRAR_API: Fetcher;
+  CATALYST_DATA_CHANNEL_REGISTRAR_API: Service<AuthzedWorker>;
+  AUTHX_TOKEN_API: Service<JWTWorker>;
 }
