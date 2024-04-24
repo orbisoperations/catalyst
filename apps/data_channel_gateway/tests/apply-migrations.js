6a0e95cd2ac5faef1b6b332d3819b64065a16050
Bert Stover <b.stover@orbisops.com>
2024-04-24 10:16:54 -0400

    removal of randomly generated build files.

--- apps/data_channel_gateway/tests/apply-migrations.js
+++
@@ -1,5 +0,0 @@
-export {};
-// console.log(`INFO: Applying ${env.TEST_SEED_MIGRATIONS.length} schema migrations:`)
-//await applyD1Migrations(env.APP_DB, env.APP_MIGRATIONS);
-// console.log(`INFO: Applying ${env.TEST_SEED_MIGRATIONS.length} seed data migrations:`)
-// await applyD1Migrations(env.APP_DB, env.TEST_SEED_MIGRATIONS);
