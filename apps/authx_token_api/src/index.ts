b106b452f1106b256e0e2fbb7ff58f5df89dfd19
Jesse Taylor <j.taylor@orbisops.com>
2024-04-29 12:14:55 -0700

    renamed HSM to JWTKeyProvider

--- apps/authx_token_api/src/index.ts
+++ apps/authx_token_api/src/index.ts
@@ -1,38 +1,36 @@
-import { DurableObjectNamespace } from "@cloudflare/workers-types"
-import {HSM} from "./do_hsm"
-import {WorkerEntrypoint} from "cloudflare:workers"
-import {Env} from "./env"
-import { JWTSigningRequest } from './jwt';
-export {HSM} from "./do_hsm"
-
+import { DurableObjectNamespace } from '@cloudflare/workers-types';
+import { JWTKeyProvider } from './durable_object_security_module';
+import { WorkerEntrypoint } from 'cloudflare:workers';
+import { Env } from './env';
+export { JWTKeyProvider } from './durable_object_security_module';
+import {  JWTSigningRequest } from '../../../packages/schema_zod';
 type Bindings = {
 	// @ts-ignore
-	HSM: DurableObjectNamespace<HSM>
-}
-
+	KEY_PROVIDER: DurableObjectNamespace<JWTKeyProvider>;
+};
 
 export default class JWTWorker extends WorkerEntrypoint<Env> {
-	async getPublicKey(keyNamespace: string = "default") {
-		const id = this.env.HSM.idFromName(keyNamespace)
-		const stub = this.env.HSM.get(id)
-		return stub.getPublicKey()
+	async getPublicKey(keyNamespace: string = 'default') {
+		const id = this.env.KEY_PROVIDER.idFromName(keyNamespace);
+		const stub = this.env.KEY_PROVIDER.get(id);
+		return stub.getPublicKey();
 	}
 
-	async rotateKey(keyNamespace: string = "default") {
-		const id = this.env.HSM.idFromName(keyNamespace)
-		const stub = this.env.HSM.get(id)
-		return stub.rotateKey()
+	async rotateKey(keyNamespace: string = 'default') {
+		const id = this.env.KEY_PROVIDER.idFromName(keyNamespace);
+		const stub = this.env.KEY_PROVIDER.get(id);
+		return stub.rotateKey();
 	}
 
 	async signJWT(jwtRequest: JWTSigningRequest, expiresIn: number, keyNamespace: string = 'default') {
-		const id = this.env.HSM.idFromName(keyNamespace);
-		const stub = this.env.HSM.get(id);
+		const id = this.env.KEY_PROVIDER.idFromName(keyNamespace);
+		const stub = this.env.KEY_PROVIDER.get(id);
 		return stub.signJWT(jwtRequest, expiresIn);
 	}
 
 	async validateToken(token: string, keyNamespace: string = 'default') {
-		const id = this.env.HSM.idFromName(keyNamespace);
-		const stub = this.env.HSM.get(id);
+		const id = this.env.KEY_PROVIDER.idFromName(keyNamespace);
+		const stub = this.env.KEY_PROVIDER.get(id);
 		return stub.validateToken(token);
 	}
 }
