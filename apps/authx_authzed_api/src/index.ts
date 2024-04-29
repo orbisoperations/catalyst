f298f0756e169f7ff0c3d4bf3c4a50c3857f47e8
jtaylor-orbis <101131172+jtaylor-orbis@users.noreply.github.com>
2024-04-29 14:02:19 -0700

    part 1 in org/user sync (#144)
     this intrdoces a new problem since authzed cant do emails as user ids
    
    we now base64 encode all userIds before being used
    
    however, we now need to do the inverse when returning anything with a user in it...

--- apps/authx_authzed_api/src/index.ts
+++ apps/authx_authzed_api/src/index.ts
@@ -13,6 +13,8 @@ type ENV = {
 	AUTHZED_PREFIX: string
 }
 
+const emailTob64 = (email: string) => {return btoa(email)}
+
 export default class AuthzedWorker extends WorkerEntrypoint<ENV> {
 	async schema(){
 		const client = new AuthzedClient(this.env.AUTHZED_ENDPOINT, this.env.AUTHZED_KEY, this.env.AUTHZED_PREFIX)
@@ -22,54 +24,55 @@ export default class AuthzedWorker extends WorkerEntrypoint<ENV> {
 
 	async addUserToOrg(orgId: OrgId, userId: UserId) {
 		const client = new AuthzedClient(this.env.AUTHZED_ENDPOINT, this.env.AUTHZED_KEY, this.env.AUTHZED_PREFIX)
-		const resp = await client.addUserToOrganization(orgId, userId)
+		const resp = await client.addUserToOrganization(orgId, emailTob64(userId))
+		console.log("synced new user", orgId, emailTob64(userId), resp)
 		return {
-			entity: `${client.utils.schemaPrefix}organization:${orgId}#user@${client.utils.schemaPrefix}user:${userId}`,
+			entity: `${client.utils.schemaPrefix}organization:${orgId}#user@${client.utils.schemaPrefix}user:${emailTob64(userId)}`,
 		...resp
 		}
 	}
 
 	async deleteUserFromOrg(orgId: OrgId, userId: UserId) {
 		const client = new AuthzedClient(this.env.AUTHZED_ENDPOINT, this.env.AUTHZED_KEY, this.env.AUTHZED_PREFIX)
-		const resp = await client.removeUserRoleFromOrganization(orgId, userId, Catalyst.RoleEnum.enum.user)
+		const resp = await client.removeUserRoleFromOrganization(orgId, emailTob64(userId), Catalyst.RoleEnum.enum.user)
 		return {
-			entity: `${client.utils.schemaPrefix}organization:${orgId}#user@${client.utils.schemaPrefix}user:${userId}`,
+			entity: `${client.utils.schemaPrefix}organization:${orgId}#user@${client.utils.schemaPrefix}user:${emailTob64(userId)}`,
 			...resp
 		}
 	}
 
 	async addDataCustodianToOrg(orgId: OrgId, userId: UserId){
 		const client = new AuthzedClient(this.env.AUTHZED_ENDPOINT, this.env.AUTHZED_KEY, this.env.AUTHZED_PREFIX)
-		const resp = await client.addDataCustodianToOrganization(orgId, userId)
+		const resp = await client.addDataCustodianToOrganization(orgId, emailTob64(userId))
 		return {
-			entity: `${client.utils.schemaPrefix}organization:${orgId}#data_custodian@${client.utils.schemaPrefix}user:${userId}`,
+			entity: `${client.utils.schemaPrefix}organization:${orgId}#data_custodian@${client.utils.schemaPrefix}user:${emailTob64(userId)}`,
 			...resp
 		}
 	}
 
 	async deleteDataCustodianFromOrg(orgId: OrgId, userId: UserId) {
 		const client = new AuthzedClient(this.env.AUTHZED_ENDPOINT, this.env.AUTHZED_KEY, this.env.AUTHZED_PREFIX)
-		const resp = await client.removeUserRoleFromOrganization(orgId, userId, Catalyst.RoleEnum.enum.data_custodian)
+		const resp = await client.removeUserRoleFromOrganization(orgId, emailTob64(userId), Catalyst.RoleEnum.enum.data_custodian)
 		return {
-			entity: `${client.utils.schemaPrefix}organization:${orgId}#user@${client.utils.schemaPrefix}user:${userId}`,
+			entity: `${client.utils.schemaPrefix}organization:${orgId}#user@${client.utils.schemaPrefix}user:${emailTob64(userId)}`,
 			...resp
 		}
 	}
 
 	async addAdminToOrg(orgId: OrgId, userId: UserId) {
 		const client = new AuthzedClient(this.env.AUTHZED_ENDPOINT, this.env.AUTHZED_KEY, this.env.AUTHZED_PREFIX)
-		const resp = await client.addAdminToOrganization(orgId, userId)
+		const resp = await client.addAdminToOrganization(orgId, emailTob64(userId))
 		return {
-			entity: `${client.utils.schemaPrefix}organization:${orgId}#admin@${client.utils.schemaPrefix}user:${userId}`,
+			entity: `${client.utils.schemaPrefix}organization:${orgId}#admin@${client.utils.schemaPrefix}user:${emailTob64(userId)}`,
 			...resp
 		}
 	}
 
 	async deleteAdminFromOrg(orgId: OrgId, userId: UserId) {
 		const client = new AuthzedClient(this.env.AUTHZED_ENDPOINT, this.env.AUTHZED_KEY, this.env.AUTHZED_PREFIX)
-		const resp = await client.removeUserRoleFromOrganization(orgId, userId, Catalyst.RoleEnum.enum.admin)
+		const resp = await client.removeUserRoleFromOrganization(orgId, emailTob64(userId), Catalyst.RoleEnum.enum.admin)
 		return {
-			entity: `${client.utils.schemaPrefix}organization:${orgId}#user@${client.utils.schemaPrefix}user:${userId}`,
+			entity: `${client.utils.schemaPrefix}organization:${orgId}#user@${client.utils.schemaPrefix}user:${emailTob64(userId)}`,
 			...resp
 		}
 	}
@@ -77,7 +80,7 @@ export default class AuthzedWorker extends WorkerEntrypoint<ENV> {
 	async listUsersInOrg(orgId: OrgId, userId?: UserId, roles?: Catalyst.RoleEnum[]) {
 		const client = new AuthzedClient(this.env.AUTHZED_ENDPOINT, this.env.AUTHZED_KEY, this.env.AUTHZED_PREFIX)
 		const resp = await client.listUsersInOrganization(orgId, {
-			userId,
+			userId: userId ? emailTob64(userId) : undefined,
 			roles
 		})
 		return resp
@@ -111,24 +114,24 @@ export default class AuthzedWorker extends WorkerEntrypoint<ENV> {
 
 	async isMemberOfOrg(orgId: OrgId, userId: UserId) {
 		const client = new AuthzedClient(this.env.AUTHZED_ENDPOINT, this.env.AUTHZED_KEY, this.env.AUTHZED_PREFIX)
-		return client.organizationPermissionsCheck(orgId, userId, Catalyst.Org.PermissionsEnum.enum.member)
+		return client.organizationPermissionsCheck(orgId, emailTob64(userId), Catalyst.Org.PermissionsEnum.enum.member)
 	}
 
 	async canAssignRolesInOrg(orgId: OrgId, userId: UserId) {
 		const client = new AuthzedClient(this.env.AUTHZED_ENDPOINT, this.env.AUTHZED_KEY, this.env.AUTHZED_PREFIX)
-		return client.organizationPermissionsCheck(orgId, userId, Catalyst.Org.PermissionsEnum.enum.role_assign)
+		return client.organizationPermissionsCheck(orgId, emailTob64(userId), Catalyst.Org.PermissionsEnum.enum.role_assign)
 	}
 
 	async canCreateUpdateDeleteDataChannel(orgId: OrgId, userId: UserId) {
 		const client = new AuthzedClient(this.env.AUTHZED_ENDPOINT, this.env.AUTHZED_KEY, this.env.AUTHZED_PREFIX)
-		return await client.organizationPermissionsCheck(orgId, userId, Catalyst.Org.PermissionsEnum.enum.data_channel_create)
-			&& await client.organizationPermissionsCheck(orgId, userId, Catalyst.Org.PermissionsEnum.enum.data_channel_update)
-			&& await client.organizationPermissionsCheck(orgId, userId, Catalyst.Org.PermissionsEnum.enum.data_channel_delete)
+		return await client.organizationPermissionsCheck(orgId, emailTob64(userId), Catalyst.Org.PermissionsEnum.enum.data_channel_create)
+			&& await client.organizationPermissionsCheck(orgId, emailTob64(userId), Catalyst.Org.PermissionsEnum.enum.data_channel_update)
+			&& await client.organizationPermissionsCheck(orgId, emailTob64(userId), Catalyst.Org.PermissionsEnum.enum.data_channel_delete)
 	}
 
 	async canReadDataChannel(orgId: OrgId, userId: UserId) {
 		const client = new AuthzedClient(this.env.AUTHZED_ENDPOINT, this.env.AUTHZED_KEY, this.env.AUTHZED_PREFIX)
-		return await client.organizationPermissionsCheck(orgId, userId, Catalyst.Org.PermissionsEnum.enum.data_channel_read)
+		return await client.organizationPermissionsCheck(orgId, emailTob64(userId), Catalyst.Org.PermissionsEnum.enum.data_channel_read)
 	}
 
 	async addOrgToDataChannel(dataChannelId: DataChannelId, orgId: OrgId) {
@@ -148,6 +151,6 @@ export default class AuthzedWorker extends WorkerEntrypoint<ENV> {
 
 	async canReadFromDataChannel(dataChannelId: DataChannelId, userId: UserId) {
 		const client = new AuthzedClient(this.env.AUTHZED_ENDPOINT, this.env.AUTHZED_KEY, this.env.AUTHZED_PREFIX)
-		return await client.dataChannelPermissionsCheck(dataChannelId, userId, Catalyst.DataChannel.PermissionsEnum.enum.read)
+		return await client.dataChannelPermissionsCheck(dataChannelId, emailTob64(userId), Catalyst.DataChannel.PermissionsEnum.enum.read)
 	}
 }
