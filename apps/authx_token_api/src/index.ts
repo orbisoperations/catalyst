2101636952a98787c46fc5de4ed6571da0e05f07
Bert Stover <24830772+stoverb@users.noreply.github.com>
2024-04-29 16:47:41 -0400

    moved jwt to rpc (#141)

--- apps/authx_token_api/src/index.ts
+++ apps/authx_token_api/src/index.ts
@@ -1,26 +1,27 @@
-import { DurableObjectNamespace } from '@cloudflare/workers-types';
-import { HSM } from './do_hsm';
-import { WorkerEntrypoint } from 'cloudflare:workers';
-import { Env } from './env';
+import { DurableObjectNamespace } from "@cloudflare/workers-types"
+import {HSM} from "./do_hsm"
+import {WorkerEntrypoint} from "cloudflare:workers"
+import {Env} from "./env"
 import { JWTSigningRequest } from './jwt';
-export { HSM } from './do_hsm';
+export {HSM} from "./do_hsm"
 
 type Bindings = {
 	// @ts-ignore
-	HSM: DurableObjectNamespace<HSM>;
-};
+	HSM: DurableObjectNamespace<HSM>
+}
+
 
 export default class JWTWorker extends WorkerEntrypoint<Env> {
-	async getPublicKey(keyNamespace: string = 'default') {
-		const id = this.env.HSM.idFromName(keyNamespace);
-		const stub = this.env.HSM.get(id);
-		return stub.getPublicKey();
+	async getPublicKey(keyNamespace: string = "default") {
+		const id = this.env.HSM.idFromName(keyNamespace)
+		const stub = this.env.HSM.get(id)
+		return stub.getPublicKey()
 	}
 
-	async rotateKey(keyNamespace: string = 'default') {
-		const id = this.env.HSM.idFromName(keyNamespace);
-		const stub = this.env.HSM.get(id);
-		return stub.rotateKey();
+	async rotateKey(keyNamespace: string = "default") {
+		const id = this.env.HSM.idFromName(keyNamespace)
+		const stub = this.env.HSM.get(id)
+		return stub.rotateKey()
 	}
 
 	async signJWT(jwtRequest: JWTSigningRequest, expiresIn: number, keyNamespace: string = 'default') {
