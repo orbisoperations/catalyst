cb29256b39678de05227ac73f57507d8b92b4a5c
Bert Stover <b.stover@orbisops.com>
2024-04-29 16:28:21 -0400

    Issued_JWT_Registry complete with testing; retrieval of these DOs will be by org_id as the DO id by name.

--- apps/data_channel_gateway/src/env.d.ts
+++ apps/data_channel_gateway/src/env.d.ts
@@ -1,6 +1,6 @@
 import RegistrarWorker from "@catalyst/data_channel_registrar/src/worker"
 import JWTWorker from '@catalyst/authx_token_api/src';
-interface Env  {
+export interface Env  {
   DATA_CHANNEL_REGISTRAR: Service<RegistrarWorker>
   AUTHX_TOKEN_API: Service<JWTWorker>
 }
