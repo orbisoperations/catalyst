55368ca3f89a0ae3f6ad889fd7c437638803beda
Mario Flores <m.floresrivera@orbisops.com>
2024-05-01 15:09:39 -0400

    can see, send accept and decline invitations

---
+++ apps/catalyst-ui/app/actions/partners.ts
@@ -0,0 +1,49 @@
+"use server";
+import { getRequestContext } from "@cloudflare/next-on-pages";
+
+function getMatcher() {
+  // @ts-ignore
+  return getRequestContext().env.ORGANIZATION_MATCHMAKING;
+}
+
+export async function listInvites(token: string) {
+  const matcher = getMatcher();
+  const result = await matcher.listInvites({ cfToken: token });
+  if (result.success) {
+    return result.invite;
+  }
+  console.error("List Invite Error:", result.error);
+  return [];
+}
+
+export async function sendInvite(receivingOrg: string, token: string) {
+  const matcher = getMatcher();
+  const result = await matcher.sendInvite(receivingOrg, { cfToken: token });
+  if (result.success) {
+    return result.invite;
+  }
+  console.error("Send Invite Error:", result.error);
+  return undefined;
+}
+
+export async function declineInvite(inviteId: string, token: string) {
+  const matcher = getMatcher();
+  console.log({ inviteId, token });
+  const result = await matcher.declineInvite(inviteId, { cfToken: token });
+  if (result.success) {
+    return result.invite;
+  }
+  console.error("Decline Invite Error:", result.error);
+  return undefined;
+}
+
+export async function acceptInvite(inviteId: string, token: string) {
+  const matcher = getMatcher();
+  const result = await matcher.acceptInvite(inviteId, { cfToken: token });
+
+  if (result.success) {
+    return result.invite;
+  }
+  console.error("Accept Invite Error:", result.error);
+  return undefined;
+}
