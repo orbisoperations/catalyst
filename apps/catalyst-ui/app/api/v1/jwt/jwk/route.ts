7f6f2f4a540e2af0c9d60249b67de87612959e20
Jesse Taylor <j.taylor@orbisops.com>
2024-05-03 09:47:07 -0700

    adding ts-ignore for service binding type issue

--- apps/catalyst-ui/app/api/v1/jwt/jwk/route.ts
+++ apps/catalyst-ui/app/api/v1/jwt/jwk/route.ts
@@ -4,6 +4,7 @@ export const dynamic = "force-dynamic"; // defaults to auto
 
 export const runtime = "edge";
 export async function GET(request: NextRequest) {
+    // @ts-ignore
     const { AUTHX_TOKEN_API : tokenAPI } = getRequestContext()
     .env as CloudflareEnv;
   
