4674a21446d712cbd1b38e196eb3702145a16676
jtaylor-orbis <101131172+jtaylor-orbis@users.noreply.github.com>
2024-05-03 09:13:35 -0700

    upgraded algo and tests for jwt (#202)

--- apps/data_channel_gateway/tests/jwt.spec.ts
+++ apps/data_channel_gateway/tests/jwt.spec.ts
@@ -5,19 +5,24 @@ import { env } from 'cloudflare:test';
 
 describe("jwt integration tests", () => {
   it("can get the public key", async () =>{
-    const pkey = await env.AUTHX_TOKEN_API.getPublicKey()
+    console.log(env)
+    const jwtDoId = env.JWT_TOKEN_DO.idFromName("default")
+    const jwtStub = env.JWT_TOKEN_DO.get(jwtDoId)
+    const pkey = await jwtStub.getPublicKey()
     expect(pkey).toBeDefined()
     expect(pkey).toBeTypeOf("object")
     expect(pkey.pem).toBeTypeOf("string")
   })
   it("can rotate the key", async () => {
-    const pkey1 = await env.AUTHX_TOKEN_API.getPublicKey()
+    const jwtDoId = env.JWT_TOKEN_DO.idFromName("default")
+    const jwtStub = env.JWT_TOKEN_DO.get(jwtDoId)
+    const pkey1 = await jwtStub.getPublicKey()
     expect(pkey1).toBeDefined()
     expect(pkey1).toBeTypeOf("object")
     expect(pkey1.pem).toBeTypeOf("string")
 
-    expect(await env.AUTHX_TOKEN_API.rotateKey()).toBeTruthy()
-    const pkey2 = await env.AUTHX_TOKEN_API.getPublicKey()
+    expect(await jwtStub.rotateKey()).toBeTruthy()
+    const pkey2 = await jwtStub.getPublicKey()
     expect(pkey2).toBeDefined()
     expect(pkey2).toBeTypeOf("object")
     expect(pkey2.pem).toBeTypeOf("string")
@@ -33,14 +38,14 @@ describe("jwt integration tests", () => {
     const jwtStub = env.JWT_TOKEN_DO.get(jwtDoId)
     const jwtToken = await jwtStub.signJWT(jwtRequest, 1000000000)
     console.log(jwtToken)
-    const validateResp = await env.AUTHX_TOKEN_API.validateToken(jwtToken.token)
+    const validateResp = await jwtStub.validateToken(jwtToken.token)
     delete validateResp["Symbol(dispose)"]
     console.log(validateResp)
     expect(validateResp.claims[0]).toBe( 'testclaim' )
     expect(validateResp.valid).toBeTruthy()
     expect(validateResp.entity).toBe("testuser")
 
-    const invalid = await env.AUTHX_TOKEN_API.validateToken(jwtToken.token + "makebad")
+    const invalid = await jwtStub.validateToken(jwtToken.token + "makebad")
     expect(invalid.valid).toBeFalsy()
   })
 })
