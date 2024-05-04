05ba64d9b23fa0233069e6ddb1ba02577d7b77b2
j.taylor <j.taylor@orbisops.com>
2024-05-04 15:07:20 +0000

    adding in user check to jwt rotate

--- apps/authx_token_api/src/index.ts
+++ apps/authx_token_api/src/index.ts
@@ -30,9 +30,33 @@ export default class JWTWorker extends WorkerEntrypoint<Env> {
 				error: "catalyst did not receive a user credential"
 			})
 		}
+		const userResp = await this.env.USERCACHE.getUser(token.cfToken)
+		if (!userResp) {
+			return JWTRotateResponse.parse({
+				success: false,
+				error: "catalyst did not find a user for the given credential"
+			})
+		}
+		const userParse = User.safeParse(userResp)
+		if (!userParse.success) {
+			console.error(userParse.error)
+			return JWTRotateResponse.parse({
+				success: false,
+				error: "catalyst was not able to access user for the given credential"
+			})
+		}
+		// add authzed here when available 
+		if (!userParse.data.zitadelRoles.includes("platform-admin")) {
+			return JWTRotateResponse.parse({
+				success: false,
+				error: "catalyst asserts user does not have access jwt admin functions"
+			})
+		}
+		
 		const id = this.env.KEY_PROVIDER.idFromName(keyNamespace);
 		const stub = this.env.KEY_PROVIDER.get(id);
 		const success = await stub.rotateKey();
+
 		if (success) {
 			return JWTRotateResponse.parse({success: success})
 		} else {
