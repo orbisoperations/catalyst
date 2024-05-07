91b3aa7c3074a0701acc7b3ba6ba1b9c44e46b31
mfromoops <101655499+mfromoops@users.noreply.github.com>
2024-05-07 19:05:28 -0400

    fixes bugs in list tokens (#265)
    ### TL;DR
    Updated the Create Tokens Form and List Tokens components.
    
    ### What changed?
    - Added fields for API key name and description in Create Tokens Form
    - Updated functionality for Create Tokens Form (its disabled for now)
    - Added apiKeyName and apiKeyDescription states in Create Tokens Form
    - Updated List Tokens component
    - Updated service binding in wrangler.toml

--- apps/catalyst-ui/app/actions/i-jwt-registry.ts
+++ apps/catalyst-ui/app/actions/i-jwt-registry.ts
@@ -1,18 +1,18 @@
 "use server";
-import { getRequestContext } from '@cloudflare/next-on-pages';
-import { IssuedJWTRegistry } from '../../../../packages/schema_zod';
+import { getRequestContext } from "@cloudflare/next-on-pages";
+import { IssuedJWTRegistry } from "../../../../packages/schema_zod";
 
 function getMatcher() {
-	// @ts-ignore
-	return getRequestContext().env.ISSUED_JWT_REGISTRY
+  // @ts-ignore
+  return getRequestContext().env.ISSUED_JWT_WORKER;
 }
 
 export async function listIJWTRegistry(token: string) {
-	const matcher = getMatcher();
-	try {
-		return (await matcher.list({ cfToken: token })) as IssuedJWTRegistry[];
-	} catch (error) {
-		console.error("List IssuedJWTRegistry Error:", error);
-		return [];
-	}
+  const matcher = getMatcher();
+  try {
+    return (await matcher.list({ cfToken: token })) as IssuedJWTRegistry[];
+  } catch (error) {
+    console.error("List IssuedJWTRegistry Error:", error);
+    return [];
+  }
 }
