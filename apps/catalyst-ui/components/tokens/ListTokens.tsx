246567db47ca946164d00563b29246aba1f29fd1
Mario Flores <m.floresrivera@orbisops.com>
2024-05-10 16:22:21 -0400

    error handling in token components

--- apps/catalyst-ui/components/tokens/ListTokens.tsx
+++ apps/catalyst-ui/components/tokens/ListTokens.tsx
@@ -2,6 +2,7 @@
 import { rotateJWTKeyMaterial } from "@/app/actions/tokens";
 import {
   CreateButton,
+  ErrorCard,
   OpenButton,
   OrbisButton,
   OrbisCard,
@@ -10,7 +11,7 @@ import {
 import { ListView } from "@/components/layouts";
 import { navigationItems } from "@/utils/nav.utils";
 import { IssuedJWTRegistry } from "@catalyst/schema_zod";
-import { Flex } from "@chakra-ui/layout";
+import { Box, Flex } from "@chakra-ui/layout";
 import { Card, CardBody, Text } from "@chakra-ui/react";
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
@@ -26,33 +27,43 @@ export default function APIKeysComponent({
   const router = useRouter();
   const { user, token } = useUser();
   const [adminFlag, setAdminFlag] = useState<boolean>(false);
-
+  const [hasError, setHasError] = useState<boolean>(false);
+  const [errorMessage, setErrorMessage] = useState<string>("");
   const [issuedJWTRegistry, setIssuedJWTRegistry] = useState<any[]>([]);
-  useEffect(() => {
+
+  function fetchIssuedJWTRegistry() {
+    setHasError(false);
     if (user !== undefined && token !== undefined) {
-      setAdminFlag(true);
+      setAdminFlag(user && token && user?.custom.isPlatformAdmin);
       listIJWTRegistry(token)
         .then((data) => {
           setIssuedJWTRegistry(data as IssuedJWTRegistry[]);
         })
         .catch((e) => {
+          setHasError(true);
+          setErrorMessage(
+            "An error occurred while fetching the tokens. Please try again later."
+          );
           console.error(e);
         });
     } else {
       setAdminFlag(false);
     }
-  }, [user, token]);
+  }
+  useEffect(fetchIssuedJWTRegistry, [user, token]);
 
   return (
     <ListView
       actions={
-        <Flex gap={5}>
-          <CreateButton
-            onClick={() => {
-              router.push("/tokens/create");
-            }}
-          />
-        </Flex>
+        !hasError ? (
+          <Flex gap={5}>
+            <CreateButton
+              onClick={() => {
+                router.push("/tokens/create");
+              }}
+            />
+          </Flex>
+        ) : undefined
       }
       topbaractions={navigationItems}
       headerTitle={{
@@ -62,68 +73,79 @@ export default function APIKeysComponent({
       topbartitle="API Keys"
       subtitle="Access Data through your own means"
       table={
-        issuedJWTRegistry.length > 0 ? (
-          <Card variant={"outline"} shadow={"md"}>
-            <OrbisTable
-              headers={["Name", "Description", "Expiration", "Owner"]}
-              rows={issuedJWTRegistry.map(
-                (jwt: {
-                  id: string;
-                  name: string;
-                  description: string;
-                  claims: string[];
-                  expiry: number;
-                  organization: string;
-                }) => {
-                  return [
-                    <Flex key={jwt.id} justifyContent={"space-between"}>
-                      <OpenButton
-                        onClick={() => router.push("/tokens/" + jwt.id)}
-                      >
-                        {jwt.name}
-                      </OpenButton>
-                    </Flex>,
-                    jwt.description,
-                    new Date(jwt.expiry).toLocaleDateString(),
-                    jwt.organization,
-                  ];
-                }
-              )}
-            />
-          </Card>
+        hasError ? (
+          <ErrorCard
+            title="Error"
+            message={errorMessage}
+            retry={fetchIssuedJWTRegistry}
+          />
         ) : (
-          <Card>
-            <CardBody>
-              No tokens exist for{" "}
-              {user !== undefined ? user.custom.org : "this user"}!
-            </CardBody>
-          </Card>
+          <Box>
+            {adminFlag && (
+              <>
+                <OrbisCard title="JWT Admin Pannel" mb={5}>
+                  <Text>JWT Admin Actions</Text>
+                  <OrbisButton
+                    onClick={async () => {
+                      if (!token) return;
+                      rotateJWTKeyMaterial(token)
+                        .then((res) => {
+                          console.log(res);
+                        })
+                        .catch((e) => {
+                          setHasError(true);
+                          setErrorMessage(
+                            "An error occurred while rotating the key. Please try again later."
+                          );
+                          console.error("error rotating keys: ", e);
+                        });
+                    }}
+                  >
+                    Rotate JWT Signing Material
+                  </OrbisButton>
+                </OrbisCard>
+              </>
+            )}
+            {issuedJWTRegistry.length > 0 ? (
+              <Card variant={"outline"} shadow={"md"}>
+                <OrbisTable
+                  headers={["Name", "Description", "Expiration", "Owner"]}
+                  rows={issuedJWTRegistry.map(
+                    (jwt: {
+                      id: string;
+                      name: string;
+                      description: string;
+                      claims: string[];
+                      expiry: number;
+                      organization: string;
+                    }) => {
+                      return [
+                        <Flex key={jwt.id} justifyContent={"space-between"}>
+                          <OpenButton
+                            onClick={() => router.push("/tokens/" + jwt.id)}
+                          >
+                            {jwt.name}
+                          </OpenButton>
+                        </Flex>,
+                        jwt.description,
+                        new Date(jwt.expiry).toLocaleDateString(),
+                        jwt.organization,
+                      ];
+                    }
+                  )}
+                />
+              </Card>
+            ) : (
+              <Card>
+                <CardBody>
+                  No tokens exist for{" "}
+                  {user !== undefined ? user.custom.org : "this user"}!
+                </CardBody>
+              </Card>
+            )}
+          </Box>
         )
       }
-    >
-      {user && token && user?.custom.isPlatformAdmin ? (
-        <>
-          <OrbisCard title="JWT Admin Pannel">
-            <Text>JWT Admin Actions</Text>
-            <OrbisButton
-              onClick={async () => {
-                console.log("rotating jwt material");
-                rotateJWTKeyMaterial(token)
-                  .then((res) => {
-                    console.log(res);
-                  })
-                  .catch((e) => {
-                    console.error("error rotating keys: ", e);
-                  });
-              }}
-            >
-              Rotate JWT Signing Material
-            </OrbisButton>
-          </OrbisCard>
-        </>
-      ) : (
-        <></>
-      )}
-    </ListView>
+    ></ListView>
   );
 }
