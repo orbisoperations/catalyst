2c02d002661d49e894bf615e43451966d0acefdb
Geoff Seemueller <g.seemueller@orbisops.com>
2024-05-10 09:16:53 -0400

    Moved channel-controller-ui to apps/ and setup realtime between server/client

---
+++ apps/datachannel-next/app/api/get-query/route.ts
@@ -0,0 +1,30 @@
+import { NextResponse } from "next/server";
+
+export async function POST(request: Request) {
+
+
+    // const resp = await fetch(url, {
+    //   method: "POST",
+    //   headers: {
+    //     "Content-Type": "application/json",
+    //     Authorization: `Bearer ${apiKey}`,
+    //   },
+    //   body,
+    // })
+    //   .then((res) => {
+    //     try {
+
+    //         return res.text()
+    //     } catch {
+    //         return res.text();
+    //     }
+    // })
+    //   .then((data) => {
+    //     console.log(data);
+    //     return data;
+    //   });
+    // console.log(request.json())
+    // const resp = await request.text()
+    // console.log(resp)
+    // return NextResponse.json({resp})
+}
