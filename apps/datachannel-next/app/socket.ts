447ebe25848b83ea9e595f80c481c6650a629a84
Geoff Seemueller <g.seemueller@orbisops.com>
2024-05-11 21:38:18 -0400

    data persists

--- apps/datachannel-next/app/socket.ts
+++ apps/datachannel-next/app/socket.ts
@@ -3,10 +3,11 @@
   This file makes socket-io work. The socket-io server is in the run.ts file at the root of this repository.
 
   Note: We need socket-io for a couple of reasons:
-  1. NextJS v14 app router does not natively support graphql-yoga graphql subscriptions.
+  1. Next 14 app router does not natively support graphql-yoga graphql subscriptions
+  2. It's awesome
  */
-
-
 import { io } from "socket.io-client";
 
-export const socket = io();
+export const socket = io({
+  //   connection options
+});
