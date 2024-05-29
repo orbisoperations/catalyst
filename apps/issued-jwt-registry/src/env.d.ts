92c7e20c5455b9d4ca804c25da210eb3695c433c
Jesse Taylor <j.taylor@orbisops.com>
2024-05-29 15:38:33 -0700

    adding tests to jwt revocation

--- apps/issued-jwt-registry/src/env.d.ts
+++ apps/issued-jwt-registry/src/env.d.ts
@@ -1,4 +1,7 @@
+import { I_JWT_Registry_DO } from ".";
 
  interface Env  {
-	ISSUED_JWT_REGISTRY_DO: DurableObjectNamespace
+	ISSUED_JWT_REGISTRY_DO: DurableObjectNamespace<I_JWT_Registry_DO>
+	USERCACHE: Service<UserCredsCacheWorker>;
+
 }
