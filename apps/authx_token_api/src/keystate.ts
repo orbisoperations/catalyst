ca64916bc90a37b74ac8e97d5b41dfc65e6a099d
Jesse Taylor <j.taylor@orbisops.com>
2024-05-30 18:13:58 -0700

    fixing jwt bug

--- apps/authx_token_api/src/keystate.ts
+++ apps/authx_token_api/src/keystate.ts
@@ -4,6 +4,20 @@ import {DEFAULT_STANDARD_DURATIONS} from "../../../packages/schema_zod"
 
 export const KEY_ALG = 'EdDSA'
 
+/*
+RFC 7519 (JWT)
+
+NumericDate
+      A JSON numeric value representing the number of seconds from
+      1970-01-01T00:00:00Z UTC until the specified UTC date/time,
+      ignoring leap seconds.  This is equivalent to the IEEE Std 1003.1,
+      2013 Edition [POSIX.1] definition "Seconds Since the Epoch", in
+      which each day is accounted for by exactly 86400 seconds, other
+      than that non-integer values can be represented.  See RFC 3339
+      [RFC3339] for details regarding date/times in general and UTC in
+      particular.
+
+ */
 
 
 export interface KeyStateSerialized {
@@ -25,7 +39,7 @@ export class KeyState {
 	constructor() {
 		this.uuid = crypto.randomUUID();
 		// this is the max value
-		this.expiry = DEFAULT_STANDARD_DURATIONS.Y
+		this.expiry = DEFAULT_STANDARD_DURATIONS.Y / DEFAULT_STANDARD_DURATIONS.MS // to be S in a year
 		this.publicKey = {} as KeyLike;
 		this.privateKey = {} as KeyLike;
 		this.publicKeyPEM = '';
@@ -51,10 +65,9 @@ export class KeyState {
 		return this.expired;
 	}
 
-	async sign(jwt: JWT, expiry: number) {
-
-		const payload = jwt.payloadRaw(expiry <= this.expiry ? expiry : this.expiry);
-
+	async sign(jwt: JWT, expiresIn: number) {
+		const expiry = (expiresIn <= this.expiry ? expiresIn : this.expiry)
+		const payload = jwt.payloadRaw(expiry);
 		return new SignJWT(payload).setProtectedHeader({ alg: KEY_ALG }).sign(this.privateKey);
 	}
 
