1ff6c35bf3cc170d3966d6d6251d1bd1862a0a47
Jasmine Maugé <j.mauge@orbisops.com>
2025-04-10 00:23:09 -0500

    Migrate issued-jwt-registry from wrangler.toml to wrangler.json

--- apps/issued-jwt-registry/vitest.config.ts
+++ apps/issued-jwt-registry/vitest.config.ts
@@ -2,49 +2,45 @@
 import { defineWorkersProject } from "@cloudflare/vitest-pool-workers/config";
 import path from "node:path";
 
-
-const userCache = path.resolve("../user-credentials-cache/dist/index.js")
+const userCache = path.resolve(__dirname, "../user-credentials-cache/dist/index.js")
 
 console.info('No external services used in this project from other workspaces within @catalyst');
 
 console.info(`Setting up vite tests for the issued-jwt durable object...`)
-export default defineWorkersProject(async () => {
-
-	return {
-		esbuild: {
-			target: "ES2022"
-		},
-		optimizeDeps: {
-			entries: ['@graphql-tools/executor-http'],
-		},
-		logLevel: 'info',
-		clearScreen: false,
-		test: {
-			globalSetup: './global-setup.ts',
-			poolOptions: {
-				workers: {
-					singleWorker: true,
-					wrangler: {configPath: "./wrangler.toml"},
-					miniflare: {
-						unsafeEphemeralDurableObjects: true,
-						workers: [
-							{
-								name: "user-credentials-cache",
-								modules: true,
-								modulesRoot: path.resolve("../user-credentials-cache"),
-								scriptPath: userCache,
-								compatibilityDate: "2024-04-05",
-								compatibilityFlags: ["nodejs_compat"],
-								entrypoint: "UserCredsCacheWorker",
-								unsafeEphemeralDurableObjects: true,
-								durableObjects: {
-									CACHE: "UserCredentialCache"
-								}
+export default defineWorkersProject({
+	esbuild: {
+		target: "ES2022"
+	},
+	optimizeDeps: {
+		entries: ['@graphql-tools/executor-http'],
+	},
+	logLevel: 'info',
+	clearScreen: false,
+	test: {
+		globalSetup: './global-setup.ts',
+		poolOptions: {
+			workers: {
+				singleWorker: true,
+				wrangler: {configPath: "./wrangler.json"},
+				miniflare: {
+					unsafeEphemeralDurableObjects: true,
+					workers: [
+						{
+							name: "user-credentials-cache",
+							modules: true,
+							modulesRoot: path.resolve(__dirname, "../user-credentials-cache"),
+							scriptPath: userCache,
+							compatibilityDate: "2024-05-24",
+							compatibilityFlags: ["nodejs_compat"],
+							entrypoint: "UserCredsCacheWorker",
+							unsafeEphemeralDurableObjects: true,
+							durableObjects: {
+								CACHE: "UserCredentialCache"
 							}
-						],
-					},
+						}
+					],
 				},
 			},
 		},
-	}
+	},
 });
