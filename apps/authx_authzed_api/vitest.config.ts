5105a1082e0b06df761128b32fcbd9a109b120cc
Gabriel Santiago <g.santiago@orbisops.com>
2025-04-15 20:20:27 -0400

    authx_authzed_api: new cf envs, migrated to wrangler.jsonc, and new unit tests

--- apps/authx_authzed_api/vitest.config.ts
+++ apps/authx_authzed_api/vitest.config.ts
@@ -1,23 +1,25 @@
-import { defineWorkersConfig } from "@cloudflare/vitest-pool-workers/config";
+import { defineWorkersConfig } from '@cloudflare/vitest-pool-workers/config';
 
 export default defineWorkersConfig({
-  logLevel: "info",
-  test: {
-    poolOptions: {
-      workers: {
-        wrangler: {
-					configPath: "./wrangler.toml",
+	logLevel: 'info',
+	test: {
+		globalSetup: './global-setup.ts',
+		poolOptions: {
+			workers: {
+				wrangler: {
+					configPath: './wrangler.jsonc',
 				},
+				isolatedStorage: false,
 				singleWorker: true,
 				miniflare: {
-					compatibilityFlags: ["nodejs_compat"],
+					compatibilityFlags: ['nodejs_compat'],
 					bindings: {
-						AUTHZED_ENDPOINT: "http://localhost:8081",
-						AUTHZED_KEY: "atoken",
-						AUTHZED_PREFIX: "orbisops_catalyst_dev/"
+						AUTHZED_ENDPOINT: 'http://localhost:8443',
+						AUTHZED_KEY: 'atoken',
+						AUTHZED_PREFIX: 'orbisops_catalyst_dev/',
 					},
-				}
-      },
-    },
-  },
+				},
+			},
+		},
+	},
 });
