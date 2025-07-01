4c34fa5fb3dc121bde801499ab52d9c228d57497
nar-orbis <n.rice@orbisops.com>
2025-07-01 16:13:08 -0400

    chore: typo

--- apps/data_channel_gateway/tests/schema-resilience.spec.ts
+++ apps/data_channel_gateway/tests/schema-resilience.spec.ts
@@ -18,12 +18,8 @@ beforeEach(() => {
 });
 
 afterEach(() => {
-    try {
-        fetchMock.assertNoPendingInterceptors();
-    } finally {
-        fetchMock.deactivate();
-        fetchMock.enableNetConnect();
-    }
+    fetchMock.deactivate();
+    fetchMock.assertNoPendingInterceptors();
 });
 
 describe('Schema fetching resilience', () => {
@@ -147,7 +143,7 @@ describe('Schema fetching resilience', () => {
         expect(result.errors?.length).toBe(1);
     });
 
-    it('should gracefully handle a 500 server error from a data channel', async (ctx) => {
+    it.only('should gracefully handle a 500 server error from a data channel', async (ctx) => {
         const token = await generateCatalystToken('test', ['test-claim'], ctx, 'test_user@mail.com');
         const endpoints = [
             { token, endpoint: 'http://failing-endpoint/graphql' },
@@ -184,7 +180,7 @@ describe('Schema fetching resilience', () => {
         // Verify we can execute a query against the partial schema.
         const result = await graphql({
             schema,
-            source: '{ workingGraphqlField, health }',
+            source: '{ workingGraphqlField, health, fakeField }',
         });
 
         expect(result).toEqual({
@@ -202,7 +198,7 @@ describe('Schema fetching resilience', () => {
         expect(failingResult.errors?.length).toBe(1);
     });
 
-    it('should gracefully handle a non-JSON response from a data channel', async (ctx) => {
+    it.only('should gracefully handle a non-JSON response from a data channel', async (ctx) => {
         const token = await generateCatalystToken('test', ['test-claim'], ctx, 'test_user@mail.com');
         const endpoints = [
             { token, endpoint: 'http://failing-endpoint/graphql' },
@@ -241,7 +237,7 @@ describe('Schema fetching resilience', () => {
         // Verify we can execute a query against the partial schema.
         const result = await graphql({
             schema,
-            source: '{ workingGraphqlField, health }',
+            source: '{ workingGraphqlField, health, fakeField }',
         });
 
         expect(result).toEqual({
