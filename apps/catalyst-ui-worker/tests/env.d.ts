12f1eaa1850d4a35275b5a4d9c746c8609821735
Jasmine Maugé <j.mauge@orbisops.com>
2025-11-26 07:53:19 -0600

    chore(ui-worker): remove misplaced backend tests from UI worker

--- apps/catalyst-ui-worker/tests/env.d.ts
+++
@@ -1,9 +0,0 @@
-import IssuedJWTRegistryWorker from '@catalyst/issued-jwt-registry/src/index';
-import { OrganizationMatchmakingDO } from '../../organization_matchmaking/src';
-declare module 'cloudflare:test' {
-    // Controls the type of `import("cloudflare:test").env
-    interface ProvidedEnv extends Env {
-        ISSUED_JWT_REGISTRY: Service<IssuedJWTRegistryWorker>;
-        ORG_MATCHMAKING: DurableObjectNamespace<OrganizationMatchmakingDO>;
-    }
-}
