98b15c8edb7d7afd2d3e614a152f03fc0a02ce14
Jasmine Maugé <j.mauge@orbisops.com>
2025-12-11 09:38:49 -0800

    refactor(ui): extract Sidebar component with barrel export

---
+++ apps/catalyst-ui-worker/components/layouts/Sidebar/index.tsx
@@ -0,0 +1,4 @@
+export { Sidebar } from './Sidebar';
+export { SidebarNavItem } from './SidebarNavItem';
+export { SidebarSection } from './SidebarSection';
+export { SidebarActionItem } from './SidebarActionItem';
